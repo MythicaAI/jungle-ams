7e5232e9743a8515473d1747ec6d7e98bed76b52
Jacob Repp <jacobrepp@gmail.com>
2024-05-13 14:49:29 -0700

    checkpoint allowed routes
    only works with httproute in same namespace but cross service routing is functioning

--- web/front/build.sh
+++ web/front/build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 #
 source .vars
-docker build -t $IMAGE_NAME .
+docker build --platform linux/amd64 --tag $IMAGE_NAME .
--- web/front/deploy.sh
+++ web/front/deploy.sh
@@ -7,3 +7,5 @@ source .vars
 docker tag $IMAGE_NAME:latest $LOCAL_IMAGE
 docker tag $LOCAL_IMAGE $REMOTE_IMAGE 
 docker push $REMOTE_IMAGE
+
+echo "pushed version: $COMMIT_HASH, image: $REMOTE_IMAGE"
