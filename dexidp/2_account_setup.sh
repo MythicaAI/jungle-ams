b34b99d12401546e096512a92dd1355e15ed1e9e
Jacob Repp <jacobrepp@gmail.com>
2024-05-15 17:07:14 -0700

    dexidp checkpoint

--- dexidp/2_account_setup.sh
+++ dexidp/2_account_setup.sh
@@ -17,10 +17,22 @@ op signin
 # https://developer.1password.com/docs/cli/secrets-reference-syntax
 
 # Create the secret with the loaded values
-export OP_GOOGLE_WS_CLIENT_ID=op://Private/Google-Mythica-WS-Auth/username
-export OP_GOOGLE_WS_CLIENT_SECRET=op://Private/Google-Mythica-WS-Auth/credential
+export OP_GOOGLE_WS_CLIENT_ID=op://Employee/Google-Workspace-Mytica-OIDC/username
+export OP_GOOGLE_WS_CLIENT_SECRET=op://Employee/Google-Workspace-Mytica-OIDC/credential
+
+#
+# Create the key, value secret version
+#
+kubectl -n dex delete secret/google-workspace-client
 
 op run -- kubectl -n dex create secret \
     generic google-workspace-client \
     --from-literal=client-id=$OP_GOOGLE_WS_CLIENT_ID \
     --from-literal=client-secret=$OP_GOOGLE_WS_CLIENT_SECRET
+
+#
+# Create the YAML expanded config version
+#
+kubectl -n dex delete secret/connectors
+op run --no-masking -- envsubst < connectors.yaml.template > connectors.yaml
+kubectl -n dex create secret generic connectors --from-file=connectors.yaml
