dfd0f2549a65daf8739379f16d7fe1876d977e17
Jacob Repp <jacobrepp@gmail.com>
2024-05-28 06:53:27 -0700

    edge checkpoint

--- api/edge/src/services/profilesService.ts
+++ api/edge/src/services/profilesService.ts
@@ -2,23 +2,36 @@
 import { Profile } from "../models";
 
 // A post request should not contain an id.
-export type ProfileCreationParams = Pick<Profile, "email" | "name" | "phoneNumbers">;
+export type ProfileCreationParams = Pick<Profile, "id" | "name" | "tags">;
 
 export class ProfilesService {
-  public get(id: str, name?: string): Profile {
+  public get(id: string, name?: string): Profile {
     return {
       id,
-      email: "jane@doe.com",
       name: name ?? "Jane Doe",
-      status: "Happy",
-      phoneNumbers: [],
+      created: new Date(),
+      updated: new Date(),
+      active: false,
+      tags: {},
+      base_href: "https://foo",
+      description: "",
+      email_verified: false,
+      location: "jane@doe.com",
+      login_count: 0,
     };
   }
 
   public create(params: ProfileCreationParams): Profile {
     return {
-      id: "", // Random
       ...params,
+      created: new Date(),
+      updated: new Date(),
+      active: false,
+      base_href: "https://foo",
+      description: "",
+      email_verified: false,
+      location: "jane@doe.com",
+      login_count: 0,
     };
   }
 }
