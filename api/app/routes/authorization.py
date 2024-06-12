d3bb60201708ffe8283cd0253c0e317046a78669
Jacob Repp <jacobrepp@gmail.com>
2024-06-12 07:12:16 -0700

    fix authorization parsing

--- api/app/routes/authorization.py
+++ api/app/routes/authorization.py
@@ -1,10 +1,12 @@
 from http import HTTPStatus
 from http.client import HTTPException
+from typing import Annotated
 
 from fastapi import Header
 from auth.data import get_profile
 from db.schema.profiles import Profile
 import auth.roles as roles
 
-async def current_profile(authorization: str = Header("Authorization")) -> Profile:
+
+async def current_profile(authorization: Annotated[str | None, Header()]) -> Profile:
     return get_profile(authorization)
