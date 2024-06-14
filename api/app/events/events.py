63beb015ea4fef87f7489ced34935247ff97659a
Jacob Repp <jacobrepp@gmail.com>
2024-06-14 21:57:22 +0000

    checkpoint for hautomation event handling

--- api/app/events/events.py
+++ api/app/events/events.py
@@ -48,11 +48,11 @@ class EventsSession(object):
         self.conn = None
         self.event_type_prefix = event_type_prefix
 
-    def __enter__(self) -> Self:
+    async def __aenter__(self) -> Self:
         return self
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        self.async_engine.dispose()
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        await self.async_engine.dispose()
         if self.conn:
             self.conn.close()
 
--- api/app/events/hautomation.py
+++ api/app/events/hautomation.py
@@ -2,17 +2,27 @@
 import asyncio
 import subprocess
 import os
+import logging
 
 from events.events import EventsSession
+
+# Configure logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+
+log = logging.getLogger(__name__)
 
 CONTAINER_REPO = 'us-central1-docker.pkg.dev/controlnet-407314/gke-us-central1-images'
 CONTAINER_NAME = 'darol-houdini'
 CONTAINER_TAG = 'latest'
 IMAGE_NAME = f"{CONTAINER_REPO}/{CONTAINER_NAME}:{CONTAINER_TAG}"
+IMAGE_NAME = f"hautomation"
 
 
 def run_docker(docker_command: list[str]):
     try:
+        log.info(f"running {' '.join(docker_command)}")
         result = subprocess.run(
             docker_command,
             capture_output=True,
@@ -25,9 +35,11 @@ def run_docker(docker_command: list[str]):
 
 
 def process_output(stdout, stderr, returncode):
-    print("output", stdout)
-    print("stderr", stderr)
-    print("returncode", returncode)
+    log.info(f"output {stdout}")
+    log.info(f"stderr {stderr}")
+    log.info(f"returncode {returncode}")
+    if returncode != 0:
+        raise ValueError(f"returncode was {returncode}")
 
 
 def pull_container():
@@ -35,7 +47,8 @@ def pull_container():
 
 
 def launch_container():
-    process_output(*run_docker(["docker", "run", "--rm", IMAGE_NAME, 'run.py helloworld']))
+    cmd = "hserver -S https://www.sidefx.com/license/sesinetd && hython /darol/automation/helloworld.py && hserver -Q"
+    process_output(*run_docker(["docker", "run", "--rm", "-it", IMAGE_NAME, '/bin/sh', '-c', cmd]))
 
 
 def parse_job_data(_):
@@ -45,14 +58,17 @@ def parse_job_data(_):
 async def main():
     """Async entrypoint to test worker dequeue, looks for SQL_URL
     environment variable to form an initial connection"""
-    pull_container()
+    # pull_container()
     sql_url = os.environ.get('SQL_URL', 'postgresql+asyncpg://test:test@localhost:5432/upload_pipeline')
-    sleep_interval = os.environ.get('SLEEP_INTERVAL', 1)
-    with EventsSession(sql_url, sleep_interval) as session:
+    sleep_interval = os.environ.get('SLEEP_INTERVAL', 3)
+    async with EventsSession(sql_url, sleep_interval) as session:
         async for event_id, json_data in session.ack_next():
-            await session.complete(event_id)
-            print(event_id, json_data)
-            launch_container()
+            try:
+                log.info(f"{event_id}: {json_data}")
+                launch_container()
+                await session.complete(event_id)
+            except Exception as ex:
+                log.exception("failure running container") 
 
 
 if __name__ == '__main__':
