5edae0b3859abba69699fea057078e22e76c6824
Kyrylo <117627600+KatkovKyrylo@users.noreply.github.com>
2024-07-10 23:48:50 +0300

    jest testing setup (#78)
    setup for jest and created some basic tests

---
+++ sites/jungle3/src/components/DeleteButton/index.tsx
@@ -0,0 +1,102 @@
+import React from 'react';
+import IconButton from '@mui/joy/IconButton';
+import { AxiosError } from 'axios';
+import { LucideCheck, LucideTrash, LucideX } from 'lucide-react';
+import {
+  deleteData,
+  extractValidationErrors,
+  translateError,
+} from '../../services/backendCommon.ts';
+import { useStatusStore } from '../../stores/statusStore.ts';
+import {
+  Card,
+  Modal,
+  ModalClose,
+  ModalDialog,
+  Stack,
+  Typography,
+} from '@mui/joy';
+
+interface DeleteButtonProps {
+  url: string;
+  name: string;
+  onDeleteSuccess?: () => void;
+}
+
+export const DeleteButton: React.FC<DeleteButtonProps> = ({
+  url,
+  name,
+  onDeleteSuccess,
+}) => {
+  const [open, setOpen] = React.useState(false);
+  const { addError, addWarning, setSuccess } = useStatusStore();
+
+  const handleError = (err: AxiosError) => {
+    addError(translateError(err));
+    extractValidationErrors(err).map((msg) => addWarning(msg));
+  };
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleConfirmDelete = () => {
+    deleteData<undefined>(url)
+      .then(() => {
+        setSuccess(`Deleted ${name}`);
+        if (onDeleteSuccess) {
+          onDeleteSuccess();
+        }
+      })
+      .catch((error) => {
+        handleError(error);
+      })
+      .finally(() => {
+        handleClose();
+      });
+  };
+
+  return (
+    <div>
+      <IconButton aria-label="delete" onClick={handleClickOpen}>
+        <LucideTrash />
+      </IconButton>
+      <Modal open={open} onClose={handleClose}>
+        <ModalDialog>
+          <ModalClose />
+          <Card>
+            <Typography>Are you sure you want to delete this item?</Typography>
+          </Card>
+          <Stack
+            direction={'row'}
+            spacing={2}
+            alignItems="center"
+            sx={{ flexGrow: 1, display: 'flex', alignItems: 'center' }}
+          >
+            <IconButton
+              aria-label="cancel"
+              onClick={handleClose}
+              color="primary"
+            >
+              <LucideX />
+              Cancel
+            </IconButton>
+            <IconButton
+              aria-label="confirm"
+              onClick={handleConfirmDelete}
+              color="primary"
+              autoFocus
+            >
+              <LucideCheck />
+              Confirm
+            </IconButton>
+          </Stack>
+        </ModalDialog>
+      </Modal>
+    </div>
+  );
+};
