5c9e1ecc0d566eea23299ac35bdbaef6104e03f8
kevin <kevin@mythica.ai>
2024-07-11 09:41:18 -0700

    Attach uploaded files to package

--- api/app/upload_package.py
+++ api/app/upload_package.py
@@ -11,7 +11,7 @@ from requests_toolbelt.multipart.encoder import MultipartEncoder
 packages = [
     {
         'asset_id': "b9febdba-f3e7-4668-8e96-802039d33495",
-        'version': "4.0.0",
+        'version': "7.0.0",
         'repo': "git@github.com:jamesrobinsonvfx/inspectnodedata.git",
         'directory': "houdini18.5/hda",
         'name': "Inspect Node Data",
@@ -65,8 +65,8 @@ for package in packages:
     print(f"Processing package: {package['name']}")
 
     # Check if the asset version already exists
-    url = f"{args.endpoint}/v1/assets/{package['asset_id']}/versions/{package['version']}"
-    response = requests.get(url)
+    asset_url = f"{args.endpoint}/v1/assets/{package['asset_id']}/versions/{package['version']}"
+    response = requests.get(asset_url)
     if response.status_code != 200:
         print(f"Failed to get asset version for {package['name']}")
         print(f"Request Error: {response.status_code} {response.content}")
@@ -83,11 +83,41 @@ for package in packages:
     repodir = os.path.join(tempdir, package['name'])
     repo = git.Repo.clone_from(package['repo'], repodir)
 
+    # Upload all files in the target directory
+    asset_contents = []
+    for root, dirs, files in os.walk(os.path.join(repodir, package['directory'])):
+        for file in files:
+            filepath = os.path.normpath(os.path.join(root, file))
+            print(f"Uploading file: {filepath}")
+
+            with open(filepath, 'rb') as f:
+                upload_url = f"{args.endpoint}/v1/upload/store"
+                m = MultipartEncoder(
+                    fields={'files': (file, f, 'application/octet-stream')}
+                )
+                headers = {
+                    "Authorization": f"Bearer {token}",
+                    "Content-Type": m.content_type
+                }
+                response = requests.post(upload_url, headers=headers, data=m)
+                if response.status_code != 200:
+                    print(f"Failed to upload file: {filepath}")
+                    print(f"Request Error: {response.status_code} {response.content}")
+                    continue
+
+                o = munchify(response.json())
+                asset_contents.append({
+                    'file_id': o.files[0].file_id,
+                    'file_name': o.files[0].file_name,
+                    'content_hash': o.files[0].content_hash,
+                    'size': o.files[0].size
+                })
+    
     # Create new asset version
     asset_ver_json = {
         'asset_id': package['asset_id'],
         'commit_ref': f"{package['repo']}/{repo.heads.main.commit.hexsha}",
-        'contents': {"files": []},
+        'contents': {"files": asset_contents},
         'name': package['name'],
         'description': package['description'],
         'author': args.profileId,
@@ -96,29 +126,8 @@ for package in packages:
     headers = {
         "Authorization": f"Bearer {token}"
     }   
-    response = requests.post(url, headers=headers, json=asset_ver_json)
-    if response.status_code != 200:
+    response = requests.post(asset_url, headers=headers, json=asset_ver_json)
+    if response.status_code != 201:
         print(f"Failed to create asset version for package: {package['name']}")
         print(f"Request Error: {response.status_code} {response.content}")
         continue
-
-    # Upload all files in the target directory
-    for root, dirs, files in os.walk(os.path.join(repodir, package['directory'])):
-        for file in files:
-            filepath = os.path.normpath(os.path.join(root, file))
-            print(f"Uploading file: {filepath}")
-
-            with open(filepath, 'rb') as f:
-                url = f"{args.endpoint}/v1/upload/store"
-                m = MultipartEncoder(
-                    fields={'files': ('jamesr_inspectnodedata.hda', f, 'application/octet-stream')}
-                )
-                headers = {
-                    "Authorization": f"Bearer {token}",
-                    "Content-Type": m.content_type
-                }
-                response = requests.post(url, headers=headers, data=m)
-                if response.status_code != 200:
-                    print(f"Failed to upload file: {filepath}")
-                    print(f"Request Error: {response.status_code} {response.content}")
-                    continue
