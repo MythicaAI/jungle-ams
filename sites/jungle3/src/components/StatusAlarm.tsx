98905d2c685e56ccc0815325f764e8b88f994d06
Kyrylo <117627600+KatkovKyrylo@users.noreply.github.com>
2024-07-19 22:47:19 +0300

    [jungle3] Converted notifications to Mui-Joy Snackbar (#111)

--- sites/jungle3/src/components/StatusAlarm.tsx
+++ sites/jungle3/src/components/StatusAlarm.tsx
@@ -1,25 +1,16 @@
-import {StatusStack} from "./StatusStack.tsx";
-import {LucideBell, LucideBellRing} from "lucide-react";
-import {Dropdown, MenuButton, Menu} from "@mui/joy";
-import {useStatusStore} from "../stores/statusStore.ts";
-import {useEffect, useState} from "react";
+import { StatusStack } from "./StatusStack.tsx";
+import { LucideBell } from "lucide-react";
+import { Dropdown, Menu, MenuButton } from "@mui/joy";
 
-
-export const StatusAlarm= () => {
-    const {hasStatus, clear} = useStatusStore();
-    const [closing, setClosing] = useState<boolean>(false);
-    useEffect(() => {
-        if (hasStatus() && !closing) {
-            setClosing(true);
-            setTimeout(() => clear(), 250);
-        }
-    }, [closing]);
-    return <Dropdown open={hasStatus()}>
-        <MenuButton>
-            {hasStatus() ? <LucideBellRing/> : <LucideBell/>}
-        </MenuButton>
-        <Menu placement="bottom-end">
-            <StatusStack/>
-        </Menu>
-    </Dropdown>;
+export const StatusAlarm = () => {
+  return (
+    <Dropdown>
+      <MenuButton>
+        <LucideBell />
+      </MenuButton>
+      <Menu placement="bottom-end">
+        <StatusStack />
+      </Menu>
+    </Dropdown>
+  );
 };
--- sites/jungle3/src/services/hooks/hooks.tsx
+++ sites/jungle3/src/services/hooks/hooks.tsx
@@ -1,5 +1,8 @@
+import React from "react";
+import { useStatusStore } from "../../stores/statusStore";
 import { SessionStartResponse } from "../../types/apiTypes";
 import { Auth } from "./services";
+import { Snackbar, Alert, Stack } from "@mui/joy";
 
 export const useAuthenticationActions = () => {
   const login = async (username: string) =>
@@ -16,4 +19,84 @@ export const useAuthenticationActions = () => {
     });
 
   return { login };
+};
+
+export const useNotification = () => {
+  const {
+    success,
+    warnings,
+    errors,
+    hasStatus: checkStatus,
+    clear,
+  } = useStatusStore();
+  const [open, setOpen] = React.useState(false);
+  const [hasStatus, setHasStatus] = React.useState(checkStatus());
+  const [duration] = React.useState<undefined | number>(3000);
+  const [left, setLeft] = React.useState<undefined | number>();
+  const timer = React.useRef<undefined | number>();
+  const countdown = () => {
+    timer.current = window.setInterval(() => {
+      setLeft((prev) => (prev === undefined ? prev : Math.max(0, prev - 100)));
+    }, 100);
+  };
+  React.useEffect(() => {
+    if (open && duration !== undefined && duration > 0) {
+      setLeft(duration);
+      countdown();
+    } else {
+      window.clearInterval(timer.current);
+    }
+  }, [open, duration]);
+
+  React.useEffect(() => {
+    setHasStatus(checkStatus());
+    setOpen(checkStatus());
+  }, [success, warnings, errors]);
+
+  const handlePause = () => {
+    window.clearInterval(timer.current);
+  };
+  const handleResume = () => {
+    countdown();
+  };
+
+  const getColor = () => {
+    if (success) return "success";
+    if (errors && errors.length > 0) return "danger";
+    if (warnings && warnings.length > 0) return "warning";
+    return "neutral";
+  };
+
+  return (
+    <Snackbar
+      sx={{ maxWidth: "550px" }}
+      variant="outlined"
+      color="neutral"
+      autoHideDuration={duration}
+      resumeHideDuration={left}
+      onMouseEnter={handlePause}
+      onMouseLeave={handleResume}
+      onFocus={handlePause}
+      onBlur={handleResume}
+      onUnmount={() => setLeft(undefined)}
+      open={hasStatus && open}
+      onClose={() => {
+        setOpen(false);
+        clear();
+      }}
+    >
+      <Stack gap="15px">
+        {success && <Alert color={getColor()}>{success}</Alert>}
+        {errors &&
+          errors.length > 0 &&
+          errors.map((msg) => <Alert color={getColor()}>Error: {msg}</Alert>)}
+
+        {warnings &&
+          warnings.length > 0 &&
+          warnings.map((msg) => (
+            <Alert color={getColor()}>Warning: {msg}</Alert>
+          ))}
+      </Stack>
+    </Snackbar>
+  );
 };
