fe6548f4e0bace0aaf81852fee9aad7d49fdac9d
Kyrylo Katkov <kyrylo.katkov@gmail.com>
2024-07-23 18:45:18 +0300

    [jungle3] split package editing into tabs

--- sites/jungle3/src/components/AssetEditFileList.tsx
+++ sites/jungle3/src/components/AssetEditFileList.tsx
@@ -1,129 +1,148 @@
 import {
-    FormControl,
-    FormLabel,
-    IconButton,
-    List,
-    ListItem,
-    ListItemButton,
-    ListItemDecorator,
-    Sheet,
-    Textarea, Typography
+  FormControl,
+  FormLabel,
+  IconButton,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemDecorator,
+  Sheet,
+  Textarea,
+  Typography,
 } from "@mui/joy";
 import {
-    LucideCircleMinus,
-    LucideDoorClosed,
-    LucideEdit,
-    LucideLink,
-    LucidePlusCircle,
-    LucideSave
+  LucideCircleMinus,
+  LucideDoorClosed,
+  LucideEdit,
+  LucideLink,
+  LucidePlusCircle,
+  LucideSave,
 } from "lucide-react";
-import {useNavigate} from "react-router-dom";
-import React, {useState} from "react";
-import {AssetVersionContent, AssetVersionContentMap} from "../types/apiTypes.ts";
-
+import { useNavigate } from "react-router-dom";
+import React, { useState } from "react";
+import {
+  AssetVersionContent,
+  AssetVersionContentMap,
+} from "../types/apiTypes.ts";
 
 interface AssetEditFileListProps {
-    title: string,
-    category: string,
-    fileTypeFilters: string[],
-    openUploadList: (category: string, fileFilters: string[]) => void;
-    removeFile: (file_id: string) => void;
-    files: AssetVersionContentMap
+  title: string;
+  category: string;
+  fileTypeFilters: string[];
+  openUploadList: (category: string, fileFilters: string[]) => void;
+  removeFile: (file_id: string) => void;
+  files: AssetVersionContentMap;
 }
 
 interface AssetFileEditableProps {
-    content: AssetVersionContent;
-    editing: boolean;
-    save: (path: string) => void;
-    cancel: () => void;
+  content: AssetVersionContent;
+  editing: boolean;
+  save: (path: string) => void;
+  cancel: () => void;
 }
 
-const AssetFileEditable: React.FC<AssetFileEditableProps> = (props: AssetFileEditableProps) => {
-    const [path, setPath] = useState(props.content.file_name);
-    const initialPath = props.content.file_name;
+const AssetFileEditable: React.FC<AssetFileEditableProps> = (
+  props: AssetFileEditableProps,
+) => {
+  const [path, setPath] = useState(props.content.file_name);
+  const initialPath = props.content.file_name;
 
+  const handleCancel = () => {
+    // revert to initial path, stop editing
+    props.save(initialPath);
+    props.editing = false;
+  };
 
-    const handleCancel = () => {
-        // revert to initial path, stop editing
-        props.save(initialPath);
-        props.editing = false;
-    };
+  const handleSave = () => {
+    props.save(path);
+    props.editing = false;
+  };
 
-    const handleSave = () => {
-        props.save(path);
-        props.editing = false;
-    }
-
-    return (
+  return (
+    <React.Fragment>
+      {props.editing ? (
+        <>
+          <Textarea
+            value={path}
+            onChange={(e) => setPath(e.target.value)}
+            placeholder="Enter File Name..."
+            minRows={1}
+            maxRows={3}
+          />
+          <IconButton onClick={handleSave}>
+            <LucideSave />
+          </IconButton>
+          <IconButton onClick={handleCancel}>
+            <LucideDoorClosed />
+          </IconButton>
+        </>
+      ) : (
         <React.Fragment>
-            {props.editing ? (
-                <>
-                    <Textarea
-                        value={path}
-                        onChange={(e) => setPath(e.target.value)}
-                        placeholder="Enter File Name..."
-                        minRows={1}
-                        maxRows={3}
-                    />
-                    <IconButton onClick={handleSave}><LucideSave/></IconButton>
-                    <IconButton onClick={handleCancel}><LucideDoorClosed/></IconButton>
-                </>
-            ) : (
-                <React.Fragment>
-                    <LucideEdit/>
-                    {path} ({props.content.file_id})
-                </React.Fragment>
-            )}
+          <LucideEdit />
+          {path} ({props.content.file_id})
         </React.Fragment>
-    );
+      )}
+    </React.Fragment>
+  );
 };
 
-const handleEditSave = () => {
-}
-const handleEditCancel = () => {
-}
-
+const handleEditSave = () => {};
+const handleEditCancel = () => {};
 
 export const AssetEditFileList: React.FC<AssetEditFileListProps> = (props) => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    return <FormControl>
-        <FormLabel>
-            <IconButton onClick={(e) => {
-                e.preventDefault();
-                props.openUploadList(props.category, props.fileTypeFilters)
-            }}>
-                <LucidePlusCircle />
-            </IconButton>
-            <Typography variant="soft" level="title-md" fontWeight={"bold"}>
-                {props.title}
-            </Typography>
-        </FormLabel>
-        <Sheet key={props.category} variant="outlined" sx={{borderRadius: 'sm'}}>
-            <List id={props.category} size={"sm"}>
-                {Object.values(props.files).map(file => (
-                    <ListItem key={file.file_id}>
-                        <ListItemDecorator>
-                            <IconButton
-                                onClick={() => props.removeFile(file.file_id)}>
-                                <LucideCircleMinus/>
-                            </IconButton>
-                        </ListItemDecorator>
-                        <ListItemButton role="menuitem">
-                            <AssetFileEditable
-                                content={file}
-                                editing={false}
-                                save={() => handleEditSave()}
-                                cancel={() => handleEditCancel()}/>
-                        </ListItemButton>
-                        <ListItemDecorator>
-                            <IconButton onClick={() => navigate(`/files/${file.file_id}`)}>
-                                <LucideLink/>
-                            </IconButton>
-                        </ListItemDecorator>
-                    </ListItem>))}
-            </List>
-        </Sheet>
+  return (
+    <FormControl>
+      <FormLabel>
+        <IconButton
+          onClick={(e) => {
+            e.preventDefault();
+            props.openUploadList(props.category, props.fileTypeFilters);
+          }}
+        >
+          <LucidePlusCircle />
+        </IconButton>
+        <Typography variant="soft" level="title-md" fontWeight={"bold"}>
+          {props.title}
+        </Typography>
+      </FormLabel>
+      <Sheet
+        key={props.category}
+        variant="outlined"
+        sx={{ borderRadius: "sm" }}
+      >
+        <List id={props.category} size={"sm"}>
+          {Object.values(props.files).length > 0 ? (
+            Object.values(props.files).map((file) => (
+              <ListItem key={file.file_id}>
+                <ListItemDecorator>
+                  <IconButton onClick={() => props.removeFile(file.file_id)}>
+                    <LucideCircleMinus />
+                  </IconButton>
+                </ListItemDecorator>
+                <ListItemButton role="menuitem">
+                  <AssetFileEditable
+                    content={file}
+                    editing={false}
+                    save={() => handleEditSave()}
+                    cancel={() => handleEditCancel()}
+                  />
+                </ListItemButton>
+                <ListItemDecorator>
+                  <IconButton
+                    onClick={() => navigate(`/files/${file.file_id}`)}
+                  >
+                    <LucideLink />
+                  </IconButton>
+                </ListItemDecorator>
+              </ListItem>
+            ))
+          ) : (
+            <ListItem>No {props.category} selected</ListItem>
+          )}
+        </List>
+      </Sheet>
     </FormControl>
-        ;
-}
+  );
+};
