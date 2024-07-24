bb49389eff8c995586bf64045988281b4b76d12f
Jacob Repp <jacobrepp@gmail.com>
2024-07-24 22:45:25 +0200

    top list cleanup and landing

---
+++ sites/jungle3/src/PackageView.tsx
@@ -0,0 +1,63 @@
+import {Box, Grid} from "@mui/joy";
+import {AssetVersionContent, AssetVersionResponse} from "./types/apiTypes.ts";
+import {
+  extractValidationErrors,
+  translateError,
+} from "./services/backendCommon.ts";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { AxiosError } from "axios";
+import { useStatusStore } from "./stores/statusStore.ts";
+import { api } from "./services/api";
+import PackageViewCarousel from "./components/PackageViewCarousel.tsx";
+import {PackageViewInfoPanel} from "./components/PackageViewInfoPanel.tsx";
+
+interface PackageViewProps {
+  asset_id?: string;
+  version_id?: string
+  thumbnails?
+}
+
+export const PackageView = (props: PackageViewProps) => {
+  const { addError, addWarning } = useStatusStore();
+  const [assetVersion, setAssetVersion] = useState({} as unknown as AssetVersionResponse);
+
+  const handleError = (err: AxiosError) => {
+    addError(translateError(err));
+    extractValidationErrors(err).map((msg) => addWarning(msg));
+  };
+
+  useEffect(() => {
+    if (!props.asset_id) {
+      return;
+    }
+
+    api
+      .get<AssetVersionResponse>({ path: `/assets/${props.asset_id}/versions/${props.version_id}` })
+      .then((r) => {
+        setAssetVersion(r as AssetVersionResponse);
+      })
+      .catch((err) => handleError(err));
+  }, [props.asset_id, props.version_id]);
+
+  const header = assetVersion ? (
+    <Grid container spacing={2}>
+      <Grid xs={7}>
+        <PackageViewCarousel {...assetVersion} /></Grid>
+      <Grid xs={5}>
+        <PackageViewInfoPanel {...assetVersion} />
+      </Grid>
+    </Grid>
+  ) : (
+    ""
+  );
+  const fileView = <Box>{header}</Box>;
+  const filePending = <Box>Loading</Box>;
+
+  return <div>{assetVersion ? fileView : filePending}</div>;
+};
+
+export const PackageViewWrapper: React.FC = () => {
+  const { asset_id, version_id } = useParams();
+  return <PackageView asset_id={asset_id} version_id={version_id} />;
+};
---
+++ sites/jungle3/src/components/PackageViewInfoPanel.tsx
@@ -0,0 +1,47 @@
+import {AssetVersionResponse} from "../types/apiTypes.ts";
+import {Card, Box, Stack, Typography, Divider} from "@mui/joy";
+import React from "react";
+import {LucideGitCommitVertical, LucideGithub} from "lucide-react";
+
+export const PackageViewInfoPanel: React.FC<AssetVersionResponse> = (av: AssetVersionResponse) => {
+
+  const convertCommitRefToLink = (ref: string) => {
+    if (ref && ref.startsWith("git@github.com")) {
+      const site_user_path = ref.split(":");
+      const user_repo_commit = site_user_path[1].split("/");
+      const repo = user_repo_commit[1].split('.')[0];
+      if (user_repo_commit.length == 3) {
+        return <a href={`https://github.com/${user_repo_commit[0]}/${repo}/commits/${user_repo_commit[2]}`}>
+          <LucideGitCommitVertical/>{repo} {user_repo_commit[2].substring(0, 8)}
+        </a>;
+      }
+      else if (user_repo_commit.length == 2) {
+        return <a href={`https://github.com/${user_repo_commit[0]}/${repo}/}`}>
+          <LucideGitCommitVertical/>{repo}
+        </a>;
+      }
+    }
+    return ref;
+  }
+
+  return <Card>
+    <Box>
+    <Stack>
+      <Typography level={"h4"}>
+        {av.org_name}::{av.name}
+      </Typography>
+      <Typography>
+        by {av.author_name}
+      </Typography>
+      <Typography sx={{
+                    overflow: 'hidden',
+                    textOverflow: 'ellipsis',
+                    whiteSpace: 'nowrap',
+                  }}>
+        {convertCommitRefToLink(av.commit_ref)}
+      </Typography>
+      <Divider orientation={"horizontal"} />
+      <Typography>{av.description}</Typography>
+    </Stack>
+    </Box></Card>;
+}
