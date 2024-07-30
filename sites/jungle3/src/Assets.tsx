8e88714b5555b60a0e422bdb26e0961e1f592f9c
Kyrylo Katkov <kyrylo.katkov@gmail.com>
2024-07-30 21:37:36 +0300

    [jungle3] Searchbar, sorting for all assets & top assets carousel

--- sites/jungle3/src/Assets.tsx
+++ sites/jungle3/src/Assets.tsx
@@ -1,6 +1,12 @@
 import {
   Box,
+  CircularProgress,
   Grid,
+  Input,
+  Option,
+  Select,
+  Stack,
+  Typography,
 } from "@mui/joy";
 import { useEffect, useState } from "react";
 import {
@@ -12,13 +18,22 @@ import { useGlobalStore } from "./stores/globalStore.ts";
 import { useStatusStore } from "./stores/statusStore.ts";
 import { AxiosError } from "axios";
 import { api } from "./services/api";
-import {PackageViewCard} from "./components/PackageViewCard";
+import { PackageViewCard } from "./components/PackageViewCard";
+import { LucideSearch } from "lucide-react";
+import { TopAssetsSlider } from "./components/TopAssetsCarousel/TopAssetsSlider.tsx";
+
+type SortType = "latest" | "oldest";
 
 const Assets = () => {
   const { authToken } = useGlobalStore();
   const { addError, addWarning } = useStatusStore();
+  const [search, setSearch] = useState<string>("");
+  const [sorting, setSorting] = useState<SortType>("latest");
+  const [isAllAssetsLoading, setIsAllAssetsLoading] = useState(true);
+  const [isTopAssetsLoading, setIsTopAssetsLoading] = useState(true);
 
-  const [versions, setVersions] = useState<AssetVersionResponse[]>([]);
+  const [topAssets, setTopAssets] = useState<AssetVersionResponse[]>([]);
+  const [allAssets, setAllAssets] = useState<AssetVersionResponse[]>([]);
 
   const handleError = (err: AxiosError) => {
     console.log("ERROR: ", err);
@@ -27,21 +42,82 @@ const Assets = () => {
   };
 
   useEffect(() => {
+    setIsAllAssetsLoading(true);
+    setIsTopAssetsLoading(true);
+
     api
       .get<AssetVersionResponse[]>({ path: "/assets/top", withAuth: false })
-      .then((r) => setVersions(r))
+      .then((r) => {
+        setTopAssets(r);
+        setIsTopAssetsLoading(false);
+      })
+      .catch((err) => handleError(err));
+
+    api
+      .get<AssetVersionResponse[]>({ path: "/assets/all", withAuth: false })
+      .then((r) => {
+        setAllAssets(r);
+        setIsAllAssetsLoading(false);
+      })
       .catch((err) => handleError(err));
   }, [authToken]);
 
-  return (
+  return isAllAssetsLoading || isTopAssetsLoading ? (
+    <CircularProgress />
+  ) : (
     <Box sx={{ flexGrow: 1, padding: 2 }}>
-      <Grid container spacing={2}>
-        {versions.map((av) => (
-          <Grid xs={4} key={av.asset_id + "_" + av.version.join('.')}>
-            <PackageViewCard {...av} />
-          </Grid>
-        ))}
-      </Grid>
+      <Stack direction="row" gap="10px" mb="15px">
+        <Input
+          startDecorator={<LucideSearch width="16px" />}
+          placeholder="Search all assets..."
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          sx={{ width: "90%" }}
+        />
+        <Select
+          value={sorting}
+          onChange={(_, value) => setSorting(value as SortType)}
+          sx={{
+            minWidth: "110px",
+            width: "10%",
+            height: "34px",
+            "& button": { outline: "none" },
+          }}
+        >
+          <Option value="latest">Latest</Option>
+          <Option value="oldest">Oldest</Option>
+        </Select>
+      </Stack>
+
+      <Stack mb="15px">
+        <Typography level="h4" textAlign="start">
+          Top Assets
+        </Typography>
+        <TopAssetsSlider assets={topAssets} />
+      </Stack>
+
+      <Stack>
+        <Typography level="h4" textAlign="start">
+          All Assets
+        </Typography>
+        <Grid container spacing={2}>
+          {allAssets
+            .filter((version) =>
+              version.name.toLowerCase().includes(search.toLowerCase()),
+            )
+            .sort((a, b) => {
+              const aDate = new Date(a.created).getTime();
+              const bDate = new Date(b.created).getTime();
+
+              return sorting === "oldest" ? aDate - bDate : bDate - aDate;
+            })
+            .map((av) => (
+              <Grid xs={4} key={av.asset_id + "_" + av.version.join(".")}>
+                <PackageViewCard av={av} />
+              </Grid>
+            ))}
+        </Grid>
+      </Stack>
     </Box>
   );
 };
---
+++ sites/jungle3/src/components/TopAssetsCarousel/TopAssetsSlider.tsx
@@ -0,0 +1,111 @@
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider, { CustomArrowProps } from "react-slick";
+import { Box } from "@mui/joy";
+import { LucideChevronLeft, LucideChevronRight } from "lucide-react";
+import { AssetVersionResponse } from "../../types/apiTypes";
+import { PackageViewCard } from "../PackageViewCard";
+
+import "./slickCarousel.css";
+
+const NextArrow: React.FC<CustomArrowProps> = ({ onClick }) => {
+  return (
+    <Box
+      onClick={onClick}
+      sx={{
+        position: "absolute",
+        right: "10px",
+        top: "10px",
+        zIndex: "1",
+        cursor: "pointer",
+        bgcolor: "white",
+        display: "flex",
+        borderRadius: "50%",
+        padding: "3px",
+      }}
+    >
+      <LucideChevronRight />
+    </Box>
+  );
+};
+
+const PrevArrow: React.FC<CustomArrowProps> = ({ onClick }) => {
+  return (
+    <Box
+      sx={{
+        position: "absolute",
+        left: "10px",
+        top: "10px",
+        zIndex: "1",
+        cursor: "pointer",
+        display: "flex",
+        bgcolor: "white",
+        borderRadius: "50%",
+        padding: "3px",
+      }}
+      onClick={onClick}
+    >
+      <LucideChevronLeft />
+    </Box>
+  );
+};
+
+type Props = {
+  assets: AssetVersionResponse[];
+};
+
+export const TopAssetsSlider: React.FC<Props> = ({ assets }) => {
+  const settings = {
+    dots: false,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    initialSlide: 0,
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+  return assets && assets.length > 0 ? (
+    <Box component="div" className="slider-container">
+      <Slider {...settings}>
+        {assets.map((asset) => (
+          <Box key={asset.asset_id}>
+            <PackageViewCard
+              sxStyles={{
+                minHeight: "200px",
+                justifyContent: "flex-end",
+              }}
+              av={asset}
+              isTopAsset
+            />
+          </Box>
+        ))}
+      </Slider>
+    </Box>
+  ) : null;
+};
