bb8e569b5dbfb10895ca1eba71484649bb15ae5e
Kyrylo <117627600+KatkovKyrylo@users.noreply.github.com>
2024-08-02 01:24:24 +0300

    [jungle3] Searchbar, sorting for all assets & top assets carousel (#144)
    * [jungle3] Searchbar, sorting for all assets & top assets carousel
    
    * [jungle3] mobile responsiveness for index page

--- sites/jungle3/src/components/PackageViewCard/index.tsx
+++ sites/jungle3/src/components/PackageViewCard/index.tsx
@@ -1,66 +1,92 @@
-import {Box, Card, CardContent, CardCover, Chip, IconButton, Typography} from "@mui/joy";
-import {getThumbnailImg} from "../../lib/packagedAssets.tsx";
-import {DownloadButton} from "../DownloadButton";
-import {LucideInfo, LucidePackage} from "lucide-react";
-import {Link, useNavigate} from "react-router-dom";
+import {
+  Box,
+  Card,
+  CardContent,
+  CardCover,
+  Chip,
+  IconButton,
+  Typography,
+} from "@mui/joy";
+import { getThumbnailImg } from "../../lib/packagedAssets.tsx";
+import { DownloadButton } from "../DownloadButton";
+import { LucideInfo, LucidePackage } from "lucide-react";
+import { Link, useNavigate } from "react-router-dom";
 import React from "react";
-import {AssetVersionResponse} from "../../types/apiTypes.ts";
+import { AssetVersionResponse } from "../../types/apiTypes.ts";
+import { SxProps } from "@mui/joy/styles/types/theme";
 
-export const PackageViewCard: React.FC<AssetVersionResponse> = (av: AssetVersionResponse) => {
+type Props = {
+  av: AssetVersionResponse;
+  sxStyles?: SxProps;
+  isTopAsset?: boolean;
+};
+
+export const PackageViewCard: React.FC<Props> = ({
+  av,
+  isTopAsset,
+  sxStyles,
+}) => {
   const navigate = useNavigate();
-  return av ? <Card>
-    <CardCover>
-      <img
-        height="200"
-        src={getThumbnailImg(av)}
-        loading={"lazy"}
-        alt={av.name}
-      />
-    </CardCover>
-    <CardContent>
-      <Typography
-        level="body-lg"
-        fontWeight="lg"
-        mt={{xs: 12, sm: 18}}
-        sx={{
-          backgroundColor: 'rgba(0, 0, 0, 0.6)', // semi-translucent dark background
-          color: 'white', // white text color for better contrast
-          padding: '8px', // some padding to make it look nicer
-          borderRadius: '4px', // optional: rounded corners
-          width: '100%'
-        }}>
-        {av.org_name}::{av.name}
-        <Box
+  return av ? (
+    <Card>
+      <CardCover>
+        <img
+          height="200"
+          src={getThumbnailImg(av)}
+          loading={"lazy"}
+          alt={av.name}
+        />
+      </CardCover>
+      <CardContent sx={sxStyles ?? undefined}>
+        <Typography
+          component="span"
+          level="body-lg"
+          fontWeight="lg"
+          mt={isTopAsset ? 0 : { xs: 12, sm: 18 }}
           sx={{
-            display: 'flex',
-            justifyContent: 'space-between',
-            alignItems: 'center',
-            marginTop: '8px',
+            backgroundColor: "rgba(0, 0, 0, 0.6)", // semi-translucent dark background
+            color: "white", // white text color for better contrast
+            padding: "8px", // some padding to make it look nicer
+            borderRadius: "4px", // optional: rounded corners
+            width: "100%",
           }}
         >
-          <DownloadButton
-            file_id={av.package_id}
-            icon={<LucidePackage/>}
-          />
-          <IconButton
-            sx={{color: 'white'}}
-            onClick={() => {navigate(`/package-view/${av.asset_id}/versions/${av.version.join('.')}`)}}
+          {av.org_name}::{av.name}
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+              marginTop: "8px",
+            }}
           >
-            <LucideInfo/>
-          </IconButton>
-          <Chip
-            key={av.version.join(".")}
-            variant="soft"
-            color={"neutral"}
-            size="lg"
-            component={Link}
-            to={`/assets/${av.asset_id}/versions/${av.version.join(".")}`}
-            sx={{borderRadius: "xl"}}
-          >
-            {av.version.join(".")}
-          </Chip>
-        </Box>
-      </Typography>
-    </CardContent>
-  </Card> : "";
-}
+            <DownloadButton file_id={av.package_id} icon={<LucidePackage />} />
+            <IconButton
+              sx={{ color: "white" }}
+              onClick={() => {
+                navigate(
+                  `/package-view/${av.asset_id}/versions/${av.version.join(".")}`,
+                );
+              }}
+            >
+              <LucideInfo />
+            </IconButton>
+            <Chip
+              key={av.version.join(".")}
+              variant="soft"
+              color={"neutral"}
+              size="lg"
+              component={Link}
+              to={`/assets/${av.asset_id}/versions/${av.version.join(".")}`}
+              sx={{ borderRadius: "xl" }}
+            >
+              {av.version.join(".")}
+            </Chip>
+          </Box>
+        </Typography>
+      </CardContent>
+    </Card>
+  ) : (
+    ""
+  );
+};
