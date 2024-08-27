b6ea65f64d65b2d15afc9e11d8a8ebd4e8e4e443
kevin <kevin@mythica.ai>
2024-08-27 10:00:06 -0700

    Add simple test for material generation

--- api/houdini/tests/run_tests.sh
+++ api/houdini/tests/run_tests.sh
@@ -4,10 +4,11 @@ hython /darol/automation/render.py --output-path /output --hda-path=/darol/tests
 hython /darol/automation/render.py --output-path /output --hda-path=/darol/tests/otls/test_labs.hda
 hython /darol/automation/render.py --output-path /output --hda-path=/darol/tests/otls/test_opencl.hda
 hython /darol/automation/render.py --output-path /output --hda-path=/darol/tests/otls/test_darol.hda
+hython /darol/automation/export_material.py --output-path /output
 hserver -Q
 
 file_count=$(find /output -type f -name "*.png" | wc -l)
-if [ "$file_count" -eq 4 ] && [ ! -e /output/log.txt ]; then
+if [ "$file_count" -eq 10 ] && [ ! -e /output/log.txt ]; then
     echo "=============================================="
     echo "All tests passed"
     echo "=============================================="
