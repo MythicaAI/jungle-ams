542c13832aa2dddeec17e40a51f8478421991a57
kevin <kevin@mythica.ai>
2024-09-09 09:28:00 -0700

    Update generate mesh automation to use jobs API

--- api/app/routes/jobs/jobs.py
+++ api/app/routes/jobs/jobs.py
@@ -4,6 +4,7 @@ from http import HTTPStatus
 from typing import Any
 
 from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.sql.functions import now as sql_now
 from pydantic import BaseModel
 from sqlmodel import Session, insert, select, text
 
@@ -18,128 +19,6 @@ from db.schema.profiles import Profile
 from routes.authorization import current_profile
 
 log = logging.getLogger(__name__)
-
-
-class GenerateMeshRequest(BaseModel):
-    file_id: str
-    params: dict[str, Any]
-
-
-class GenerateMeshInterfaceResponse(BaseModel):
-    file_id: str
-
-
-class GenerateMeshResponse(BaseModel):
-    event_id: str
-
-
-class GenerateMeshState(str, Enum):
-    """Generate mesh request states"""
-    queued = "queued"
-    processing = "processing"
-    failed = "failed"
-    completed = "completed"
-
-
-class GenerateMeshStatusResponse(BaseModel):
-    state: GenerateMeshState = GenerateMeshState.queued
-    file_id: str | None = None
-
-
-router = APIRouter(prefix="/jobs", tags=["jobs"])
-
-
-def add_generate_mesh_event(session: Session, request: GenerateMeshRequest, profile_seq: int):
-    """Add a new event that triggers mesh generation"""
-    # Create a new pipeline event
-    job_data = {
-        'file_id': request.file_id,
-        'profile_id': profile_seq_to_id(profile_seq),
-        'params': request.params
-    }
-    location = app_config().mythica_location
-    stmt = insert(Event).values(
-        event_type="generate_mesh_requested",
-        job_data=job_data,
-        owner_seq=profile_seq,
-        created_in=location,
-        affinity=location)
-    event_result = session.exec(stmt)
-    log.info("generate mesh event for %s by %s -> %s",
-             request.file_id, profile_seq, event_result)
-    return event_result
-
-
-@router.get("/generate-mesh/interface/{file_id}")
-async def get_generate_mesh_interface_by_id(
-        file_id: str) -> GenerateMeshInterfaceResponse:
-    """Gets the interface definition for a file"""
-
-    with get_session() as session:
-        # HACK: Add method of associating interface json with a file
-        query = (
-            select(FileContent)
-            .where(FileContent.name.like(f"{file_id}_interface.json"))  # pylint: disable=E1101
-            .limit(1)
-        )
-
-        file = session.exec(query).one_or_none()
-        if file is not None:
-            return GenerateMeshInterfaceResponse(file_id=file_seq_to_id(file.file_seq))
-
-    return GenerateMeshInterfaceResponse(file_id="")
-
-
-@router.post("/generate-mesh")
-async def generate_mesh_file_by_id(
-        request: GenerateMeshRequest,
-        profile: Profile = Depends(current_profile)) -> GenerateMeshResponse:
-    """Generates a mesh based on the file content"""
-
-    with get_session() as session:
-        event_result = add_generate_mesh_event(session, request, profile.profile_seq)
-        session.commit()
-
-        event_seq = event_result.inserted_primary_key[0]
-        event_id = event_seq_to_id(event_seq)
-
-        return GenerateMeshResponse(event_id=event_id)
-
-
-@router.get("/generate-mesh/status/{event_id}")
-async def get_generate_mesh_status_by_id(
-        event_id: str) -> GenerateMeshStatusResponse:
-    """Gets the results of a generate mesh request"""
-
-    with get_session() as session:
-        event_seq = event_id_to_seq(event_id)
-        event = session.exec(select(Event).where(Event.event_seq == event_seq)).one_or_none()
-        if event is None:
-            raise HTTPException(HTTPStatus.NOT_FOUND, detail="event_id not found")
-
-        state = GenerateMeshState.queued
-        file_id = None
-
-        if not event.acked:
-            state = GenerateMeshState.queued
-        elif not event.completed:
-            state = GenerateMeshState.processing
-        else:
-            # HACK: Add method of associating resulting mesh with event
-            query = (
-                select(FileContent)
-                .where(FileContent.name.like(f"{event_id}_mesh.%%"))  # pylint: disable=E1101
-                .limit(1)
-            )
-
-            file = session.exec(query).one_or_none()
-            if file is not None:
-                file_id = file_seq_to_id(file.file_seq)
-                state = GenerateMeshState.completed
-            else:
-                state = GenerateMeshState.failed
-
-        return GenerateMeshStatusResponse(state=state, file_id=file_id)
 
 
 class JobDefinitionRequest(BaseModel):
@@ -173,13 +52,20 @@ class JobResultRequest(BaseModel):
     result_data: dict[str, Any]
 
 
-class JobResultResponse(BaseModel):
+class JobResultCreateResponse(BaseModel):
     job_result_id: str
 
 
 class JobResultModel(JobResultRequest):
-    job_id: str
     job_result_id: str
+
+
+class JobResultResponse(BaseModel):
+    completed: bool
+    results: list[JobResultModel]
+
+
+router = APIRouter(prefix="/jobs", tags=["jobs"])
 
 
 @router.post('/definitions', status_code=HTTPStatus.CREATED)
@@ -200,22 +86,48 @@ async def get_job_defs() -> list[JobDefinitionModel]:
         return [JobDefinitionModel(job_def_id=job_def_seq_to_id(job_def.job_def_seq), **job_def.model_dump())
                 for job_def in job_defs]
 
+def add_job_requested_event(session: Session, job_seq: int, job_def, params: str, profile_seq: int):
+    """Add a new event that triggers job processing"""
+    # Create a new pipeline event
+    job_data = {
+        'job_seq': job_seq,
+        'profile_id': profile_seq_to_id(profile_seq),
+        'config': job_def.config,
+        'params': params
+    }
+    location = app_config().mythica_location
+    stmt = insert(Event).values(
+        event_type=f"{job_def.job_type}:requested",
+        job_data=job_data,
+        owner_seq=profile_seq,
+        created_in=location,
+        affinity=location)
+    event_result = session.exec(stmt)
+    log.info("job requested for %s by %s -> %s", job_def.job_type, profile_seq, event_result)
+    return event_result
 
 @router.post('/', status_code=HTTPStatus.CREATED)
 async def create_job(
-        request: JobRequest) -> JobResponse:
+    request: JobRequest,
+    profile: Profile = Depends(current_profile)) -> JobResponse:
     with get_session() as session:
+        job_def = session.exec(select(JobDefinition).where(JobDefinition.job_def_seq == job_def_id_to_seq(request.job_def_id))).one_or_none()
+        if job_def is None:
+            raise HTTPException(HTTPStatus.NOT_FOUND, detail="job_def_id not found")
         job = session.exec(insert(Job).values(
             job_def_seq=job_def_id_to_seq(request.job_def_id),
+            owner_seq=profile.profile_seq,
             params=request.params))
         job_seq = job.inserted_primary_key[0]
+
+        add_job_requested_event(session, job_seq, job_def, request.params, profile.profile_seq)
         session.commit()
         return JobResponse(
             job_id=job_seq_to_id(job_seq),
             job_def_id=request.job_def_id)
 
 
-def job_result_insert(session: Session, job_id: str, request: JobResultRequest):
+def job_result_insert(session: Session, job_seq: int, request: JobResultRequest):
     """Generate the job result insert with fallback for databases without sequences
     support on composite primary keys"""
     from db.connection import engine
@@ -235,13 +147,13 @@ def job_result_insert(session: Session, job_id: str, request: JobResultRequest):
             params={"name": "job_result_seq_seq"}).scalar()
 
         return session.exec(insert(JobResult).values(
-            job_seq=job_id_to_seq(job_id),
+            job_seq=job_seq,
             job_result_seq=next_job_result_seq,
             created_in=request.created_in,
             result_data=request.result_data))
     else:
         return session.exec(insert(JobResult).values(
-            job_seq=job_id_to_seq(job_id),
+            job_seq=job_seq,
             created_in=request.created_in,
             result_data=request.result_data))
 
@@ -251,18 +163,44 @@ async def create_job_result(
         job_id: str,
         request: JobResultRequest) -> JobResultResponse:
     with get_session() as session:
-        job_result = job_result_insert(session, job_id, request)
+        job_seq = job_id_to_seq(job_id)
+        job = session.exec(select(Job).where(Job.job_seq == job_seq)).one_or_none()
+        if job is None:
+            raise HTTPException(HTTPStatus.NOT_FOUND, detail="job_id not found")
+
+        job_result = job_result_insert(session, job_seq, request)
         job_result_seq = job_result.inserted_primary_key[0]
         session.commit()
-        return JobResultResponse(job_result_id=job_result_seq_to_id(job_result_seq))
+        return JobResultCreateResponse(job_result_id=job_result_seq_to_id(job_result_seq))
 
 
 @router.get('/results/{job_id}')
-async def get_job_results(job_id: str) -> list[JobResultModel]:
+async def get_job_results(
+    job_id: str,
+    profile: Profile = Depends(current_profile)) -> JobResultResponse:
     with get_session() as session:
+        job_seq = job_id_to_seq(job_id)
+        job = session.exec(select(Job).where(Job.job_seq == job_seq)).one_or_none()
+        if job is None:
+            raise HTTPException(HTTPStatus.NOT_FOUND, detail="job_id not found")
+        if job.owner_seq != profile.profile_seq:
+            raise HTTPException(HTTPStatus.FORBIDDEN, detail="job_id not owned by profile")
+
         job_results = session.exec(select(JobResult)
-                                   .where(JobResult.job_seq == job_id_to_seq(job_id))).all()
-        return [JobResultModel(job_id=job_seq_to_id(job_result.job_seq),
-                               job_result_id=job_result_seq_to_id(job_result.job_result_seq),
-                               **job_result.model_dump())
-                for job_result in job_results]
+                .where(JobResult.job_seq == job_seq)).all()
+        results = [JobResultModel(job_result_id=job_result_seq_to_id(job_result.job_result_seq),
+                                **job_result.model_dump()) 
+                    for job_result in job_results]
+        return JobResultResponse(completed=(job.completed is not None), results=results)
+
+@router.post('/complete/{job_id}')
+async def set_job_completed(
+    job_id: str):
+    with get_session() as session:
+        job_result = session.exec(update(Job)
+                                  .where(Job.job_seq == job_id_to_seq(job_id))
+                                  .where(Job.completed == None)
+                                  .values(completed=sql_now()))
+        if job_result.rowcount == 0:
+            raise HTTPException(HTTPStatus.NOT_FOUND, detail="job_id not found or already completed")
+        session.commit()
--- api/app/tests/test_jobs.py
+++ api/app/tests/test_jobs.py
@@ -12,7 +12,9 @@ from tests.shared_test import assert_status_code
 
 def test_create_update(client, api_base, create_profile):
     test_profile = create_profile()
+    test_profile2 = create_profile()
     headers = test_profile.authorization_header()
+    headers2 = test_profile2.authorization_header()
 
     # Create a job definition
     r = client.post(f'{api_base}/jobs/definitions',
@@ -44,6 +46,17 @@ def test_create_update(client, api_base, create_profile):
     definitions = r.json()
     assert any([definition['job_def_id'] == job_def_id for definition in definitions])
 
+    # Create a invalid job
+    r = client.post(f'{api_base}/jobs',
+                json={
+                    'job_def_id': "INVALID",
+                    'params': {
+                        'size': 5.0
+                    }
+                },
+                headers=headers)
+    assert_status_code(r, HTTPStatus.NOT_FOUND)
+
     # Create a job
     r = client.post(f'{api_base}/jobs',
                     json={
@@ -72,7 +85,7 @@ def test_create_update(client, api_base, create_profile):
     o = munchify(r.json())
     assert 'job_result_id' in o
 
-    # Complete the job
+    # Do more work
     r = client.post(f'{api_base}/jobs/results/{job_id}',
                     json={
                         'created_in': 'houdini-worker',
@@ -82,6 +95,10 @@ def test_create_update(client, api_base, create_profile):
                     },
                     headers=headers)
     assert_status_code(r, HTTPStatus.CREATED)
+
+    # Mark the job as completed
+    r = client.post(f'{api_base}/jobs/complete/{job_id}')
+    assert_status_code(r, HTTPStatus.OK)
 
     # Get the result data
     r = client.get(f'{api_base}/jobs/results/{job_id}', headers=headers)
@@ -99,3 +116,7 @@ def test_create_update(client, api_base, create_profile):
             assert o.result_data.progress == 42
         else:
             assert False, "missing result data"
+
+    # Get the result data from another profile
+    r = client.get(f'{api_base}/jobs/results/{job_id}', headers=headers2)
+    assert_status_code(r, HTTPStatus.FORBIDDEN)
