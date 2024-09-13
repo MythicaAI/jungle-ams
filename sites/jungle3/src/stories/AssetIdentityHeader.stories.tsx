62f6d373364672e7409780567f090105cc241e59
Kyrylo Katkov <kyrylo.katkov@gmail.com>
2024-09-13 18:52:31 +0300

    cleanup

--- sites/jungle3/src/stories/AssetIdentityHeader.stories.tsx
+++ sites/jungle3/src/stories/AssetIdentityHeader.stories.tsx
@@ -2,12 +2,11 @@ import { Meta, StoryObj } from "@storybook/react";
 import { AssetIdentityHeader } from "@components/AssetIdentityHeader";
 import { useAssetVersionStore } from "@store/assetVersionStore";
 
-// Define the Storybook metadata
 const meta: Meta<typeof AssetIdentityHeader> = {
   title: "Components/AssetIdentityHeader",
   component: AssetIdentityHeader,
   parameters: {
-    layout: "centered", // You can change the layout if needed
+    layout: "centered",
   },
 };
 
@@ -15,11 +14,9 @@ export default meta;
 
 type Story = StoryObj<typeof AssetIdentityHeader>;
 
-// Define the default story
 export const Default: Story = {
   decorators: [
     (Story) => {
-      // Mock Zustand store values before rendering the story
       useAssetVersionStore.setState({
         asset_id: "1234567890",
         org_id: "org-42",
