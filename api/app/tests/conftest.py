484132055da81e54f680ea874bf510d349a8e258
Jacob Repp <jacobrepp@gmail.com>
2024-09-29 01:38:17 -0700

    move CI config to conftest

--- api/app/tests/conftest.py
+++ api/app/tests/conftest.py
@@ -1,11 +1,46 @@
+import os
+import tempfile
+
 import pytest
+import logging
+
 from fastapi.testclient import TestClient
 
 from main import app
+from config import app_config
+
+log = logging.getLogger(__name__)
+
+print("conftest-debug")
+
+@pytest.fixture(scope='package', autouse=True)
+def setup_database():
+    """
+    Set up the local database for testing
+    """
+    if os.environ.get("SQL_URL"):
+        log.info("SQL_URL detected in environment, skip setup_database fixture")
+        return
+
+    with (tempfile.TemporaryDirectory() as tmp_dir):
+        db_path = os.path.join(tmp_dir, 'mythica.db')
+        local_storage_path = os.path.join(tmp_dir, 'local_storage')
+
+        cfg = app_config()
+
+        cfg.sql_url = f"sqlite:///{db_path}"
+
+        cfg.local_storage_path = local_storage_path
+        cfg.use_local_storage = True
+        cfg.upload_folder_auto_clean = False
+
+        log.info(f"setup_database fixture configured {db_path}")
+
+        yield cfg
 
 
-@pytest.fixture(scope='session')
-def api_base() -> str:
+@pytest.fixture(scope='package')
+def api_base(setup_database) -> str:
     """Return the current api base"""
     return "/v1"
 
