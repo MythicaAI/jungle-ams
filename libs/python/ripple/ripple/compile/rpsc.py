10050c44fdae9e716ae8d0311ee0cfe99a81b837
kevin <kevin@mythica.ai>
2024-09-30 13:07:53 -0700

    Inputs tests

--- libs/python/ripple/ripple/compile/rpsc.py
+++ libs/python/ripple/ripple/compile/rpsc.py
@@ -4,11 +4,16 @@ Ripple Param Spec Compiler.
 Converts interface files or other inputs into param specs
 that can be executed by a runtime.
 """
-
+import json
 from ripple.models.params import ParameterSpec
 
 def compile_interface(interface_data: str) -> ParameterSpec:
     """
     Compile an interface file into a parameter spec.
     """
-    return ParameterSpec(inputs=[], params={})
+    data = json.loads(interface_data)
+    print(data)
+
+    inputs = data['inputLabels']
+    params = {}
+    return ParameterSpec(inputs=inputs, params=params)
--- libs/python/ripple/tests/test_params.py
+++ libs/python/ripple/tests/test_params.py
@@ -6,6 +6,35 @@ from ripple.compile.rpsc import compile_interface
 test_data_file = './tests/test_data/test_interface.json'
 
 def test_param_compile():
+    # Minimal test
+    data = """
+    {
+        "inputs": 0,
+        "params": {},
+        "inputLabels": []
+    }
+    """
+    compiled = compile_interface(data)
+    assert compiled.inputs == []
+    assert compiled.params == {}
+
+    # Inputs test
+    data = """
+    {
+        "inputs": 2,
+        "params": {},
+        "inputLabels": [
+            "Test Input 0",
+            "Test Input 1"
+        ]
+    }
+    """
+    compiled = compile_interface(data)
+    assert len(compiled.inputs) == 2
+    assert compiled.inputs[0] == "Test Input 0"
+    assert compiled.inputs[1] == "Test Input 1"
+
+    """
     # Load test data
     test_data = None
     with open(test_data_file, 'r') as f:
@@ -13,5 +42,5 @@ def test_param_compile():
 
     # Convert test data to a string
     compiled = compile_interface(test_data)
-
+    """
     assert True
