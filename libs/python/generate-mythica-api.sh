40563635cd0cba80ca9b2dcb05704273268eaf21
Jacob Repp <jacobrepp@gmail.com>
2024-10-05 07:22:17 -0700

    test-client checkpoint

--- libs/python/generate-mythica-api.sh
+++ libs/python/generate-mythica-api.sh
@@ -2,7 +2,7 @@
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 pushd $SCRIPT_DIR
-CONFIG_PATH=$SCRIPT_DIR/mythica-api-client-config.yaml
+CONFIG_PATH=$SCRIPT_DIR/mythica-api-config.yaml
 
 openapi-python-client generate \
 	--url https://api.mythica.ai/v1/openapi.json \
