9bb11b402a9c546a4d52d0c6543e05b694547d36
Jacob Repp <jacobrepp@gmail.com>
2024-10-05 07:25:48 -0700

    simplified operation IDs

--- api/app/main.py
+++ api/app/main.py
@@ -7,6 +7,7 @@ from contextlib import asynccontextmanager
 import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.routing import APIRoute
 from prometheus_fastapi_instrumentator import Instrumentator
 
 import log_config
@@ -38,8 +39,16 @@ async def server_lifespan(_: FastAPI):
         }
 
 
+def custom_generate_unique_id(route: APIRoute):
+    """Generate a simplified operation ID for client generation"""
+    return f"{route.tags[0]}-{route.name}"
+
+
+app = FastAPI()
+
 app = FastAPI(
     openapi_version='3.1.0',
+    generate_unique_id_function=custom_generate_unique_id,
     servers=[
         {'url': 'https://api.mythica.ai/', 'description': 'Production environment'},
         {'url': 'http://localhost:8080', 'description': 'Local environment'}],
