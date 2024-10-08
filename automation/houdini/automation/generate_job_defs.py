e577d844e3148241ef6edf49e79a0f5da0e4b1aa
Kevin Calderone <kevincalderone@gmail.com>
2024-10-08 07:36:21 -0700

    [houdini] Add automation for generating job defs (#276)
    * Stub in houdini job def generator implementation
    
    Cleanup
    
    Expose worker
    
    Read file_id from request
    
    Fix mismatched parameter name
    
    Fix incorrect input model type
    
    * Cleanup
    
    * Cleanup
    
    * Fix bugs
    
    * Cleanup
    
    ---------
    
    Co-authored-by: kevin <kevin@mythica.ai>

---
+++ automation/houdini/automation/generate_job_defs.py
@@ -0,0 +1,88 @@
+import hou
+import json
+import logging
+import mythica.network as mnet
+import requests
+
+from ripple.compile.rpsc import compile_interface
+from ripple.models.params import ParameterSet, ParameterSpec, FileParameterSpec, IntParameterSpec
+from typing import Optional
+
+#TODO: Configure elsewhere
+ENDPOINT = "https://api.mythica.ai/v1"
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+log = logging.getLogger(__name__)
+
+
+def extract_node_type_info(hda_path: str) -> list[dict]:
+    hou.hda.installFile(hda_path, force_use_assets=True)
+
+    result = []
+    for assetdef in hou.hda.definitionsInFile(hda_path):
+        type = assetdef.nodeType()
+        type_info = mnet.get_node_type(type, False)
+        result.append(type_info)
+
+    hou.hda.uninstallFile(hda_path)
+
+    return result
+
+
+def set_config_params(param_spec: ParameterSpec, hda_file_id: str, index: int):
+    param_spec.params['hda_file'] = FileParameterSpec(
+        label='HDA File', 
+        constant=True, 
+        default=hda_file_id
+    )
+    param_spec.params['hda_definition_index'] = IntParameterSpec(
+        label='HDA Definition Index', 
+        constant=True, 
+        default=index
+    )
+
+
+def publish_job_def(name: str, description: str, param_spec: ParameterSpec) -> Optional[str]:
+    definition = {
+        'job_type': 'houdini_generate_mesh',
+        'name': f"Generate {name}",
+        'description': description,
+        'config': {},     #TODO: Remove from request/db schema
+        'input_files': 0, #TODO: Remove from request/db schema
+        'params_schema': param_spec.dict()
+    }
+    response = requests.post(
+        f"{ENDPOINT}/jobs/definitions",
+        json=definition, timeout=10)
+
+    if response.status_code != 201:
+        log.warning("Failed to create job definition for %s. Status code: %s Error: %s",
+                    name, response.status_code, response.text)
+        return None
+    
+    result = response.json()
+    job_def_id = result['job_def_id']
+
+    return job_def_id
+
+
+def generate_job_defs(request: ParameterSet, result_callback):
+    hda_file = request.params['hda_file']
+
+    type_infos = extract_node_type_info(hda_file.file_path)
+
+    for index, type_info in enumerate(type_infos):
+        name = type_info['name']
+        description = type_info['description']
+        param_spec = compile_interface(json.dumps(type_info, indent=2))
+
+        set_config_params(param_spec, hda_file.file_id, index)
+        job_def_id = publish_job_def(name, description, param_spec)
+
+        if job_def_id is not None:
+            result_callback({
+                'message': f"Created job definition {job_def_id}"
+            })
