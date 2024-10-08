c4fcfd9b99ddd56db5f7c22cbe323fb64d2ede54
Kevin Calderone <kevincalderone@gmail.com>
2024-10-08 09:39:50 -0700

    [houdini] Fix houdini test script (#281)
    Fix houdini tests
    
    Co-authored-by: kevin <kevin@mythica.ai>

--- automation/houdini/tests/run_tests.sh
+++ automation/houdini/tests/run_tests.sh
@@ -1,9 +1,9 @@
 hserver -S https://www.sidefx.com/license/sesinetd
-hython /darol/automation/helloworld.py
-hython /darol/automation/render.py --output-path /output --hda-path=/darol/tests/otls/test_cube.hda
-hython /darol/automation/render.py --output-path /output --hda-path=/darol/tests/otls/test_labs.hda
-hython /darol/automation/render.py --output-path /output --hda-path=/darol/tests/otls/test_opencl.hda
-hython /darol/automation/render.py --output-path /output --hda-path=/darol/tests/otls/test_darol.hda
+hython /app/automation/helloworld.py
+hython /app/automation/render.py --output-path /output --hda-path=/app/tests/otls/test_cube.hda
+hython /app/automation/render.py --output-path /output --hda-path=/app/tests/otls/test_labs.hda
+hython /app/automation/render.py --output-path /output --hda-path=/app/tests/otls/test_opencl.hda
+hython /app/automation/render.py --output-path /output --hda-path=/app/tests/otls/test_darol.hda
 hserver -Q
 
 file_count=$(find /output -type f -name "*.png" | wc -l)
