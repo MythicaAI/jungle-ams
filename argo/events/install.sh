25ae8659c25435fc878f38c58cff7f6748e87227
pedromartello <pedro@mythica.ai>
2024-10-08 13:14:17 -0400

    [argo] argo-workflow installation. Disabling automation events

--- argo/events/install.sh
+++ argo/events/install.sh
@@ -2,7 +2,10 @@ kubectl config set-context --current --namespace=argo
 
 helm install argo-events argo/argo-events
 
+kubectl apply -f nats-lb.yaml
 kubectl apply -f event-bus.yaml
 kubectl apply -f event-roles.yaml
-kubectl apply -f event-sources.yaml
-kubectl apply -f event-sensors.yaml
+
+######DEPRECATED
+#kubectl apply -f event-sources.yaml
+#kubectl apply -f event-sensors.yaml
--- argo/helm_install.sh
+++
@@ -1,2 +0,0 @@
-helm repo add argo https://argoproj.github.io/argo-helm
-helm install argo argo/argo-workflows --namespace argo -f values.yaml
