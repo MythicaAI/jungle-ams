6ce7e575676113f653d3aa407e8647adc44adf02
kevin <kevin@mythica.ai>
2024-10-08 15:04:01 -0700

    WIP

--- automation/houdini/automation/export_mesh.py
+++ automation/houdini/automation/export_mesh.py
@@ -1,64 +1,29 @@
-import os
 import hou
 import json
-from pydantic import BaseModel, Field
-from typing import Literal
-from pathlib import Path
+import logging
 import mythica.darol as mdarol
+import os
+import requests
+import tempfile
+
+from pydantic import BaseModel, Field
+from pathlib import Path
+from ripple.models.params import ParameterSet, FileParameter
+from typing import Literal
+
+#TODO: Configure elsewhere
+ENDPOINT = "https://api.mythica.ai/v1"
 
 
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+log = logging.getLogger(__name__)
 
-class ExportMeshRequest(BaseModel):
-    hdapath: Path = Field(..., description="The path to the HDA (Houdini Digital Asset).")
-    output_path: Path = Field(..., description="The path where the exported mesh will be saved.")
-    output_file_name: str = Field(..., description="The name of the output file.")
-    format: Literal['fbx', 'glb', 'usdz'] = Field(..., description="The export format (fbx, glb, usdz).")
-    parms_file: Path = Field(..., description="The path to the parameters file.")
 
-    class Config:
-        schema_extra = {
-            "example": {
-                "hdapath": "/path/to/hda/file.hda",
-                "output_path": "/path/to/output/",
-                "output_file_name": "exported_mesh",
-                "format": "fbx",
-                "parms_file": "/path/to/parms/file.json"
-            }
-        }
-    
-def export_mesh(request: ExportMeshRequest, result_callback):
-    hdapath = request.hdapath
-    output_path = request.output_path
-    output_file_name= request.output_file_name 
-    format = request.format, 
-    parms_file = request.parms_file
-
-    os.makedirs(output_path, exist_ok=True)
-
-    hip = os.path.join(output_path,f'export_mesh_{os.path.basename(hdapath)}.hip')
-
-    mdarol.start_houdini(hip)
-
-    hou.hda.installFile(hdapath,force_use_assets=True)
-
-    # Load parameters from file
-    parms_data = {}
-    if parms_file:
-        with open(parms_file) as f:
-            parms_data = json.load(f)
-
-    # Geometry
-    obj = hou.node('obj')
-    geo = obj.createNode('geo','geometry')
-    stage = hou.node('stage')
-
-    # TODO: Support specifying which definition inside the hda file to use
-    assetdef = hou.hda.definitionsInFile(hdapath)[0]
-    asset = geo.createNode(assetdef.nodeTypeName())
-
-    # Set parms
-    for k, v in parms_data['mesh_parms'].items():
-        # TODO: Support ramp parameters
+def apply_params(asset, params: dict):
+    # TODO: Support ramp parameters
+    for k, v in params.items():
         if not isinstance(v, dict):
             val = [v] if not (isinstance(v, tuple) or isinstance(v, list)) else v
             parm = asset.parmTuple(k)
@@ -66,6 +31,34 @@ def export_mesh(request: ExportMeshRequest, result_callback):
                 parm.set(val)
             else:
                 print(f"Parameter {k} not found in {assetdef.nodeTypeName()}")
+
+
+def generate_mesh(
+    hda_path: str,
+    hda_definition_index: int,
+    format: str,
+    params: dict,
+    output_path: str,
+    temp_dir: str
+):
+    hda_name = os.path.basename(hda_path)
+    hip = os.path.join(temp_dir, f'export_mesh_{os.path.basename(hda_name)}.hip')
+
+    mdarol.start_houdini(hip)
+
+    hou.hda.installFile(hda_path, force_use_assets=True)
+
+    # Geometry
+    obj = hou.node('obj')
+    geo = obj.createNode('geo','geometry')
+    stage = hou.node('stage')
+
+    # TODO: Support specifying which definition inside the hda file to use
+    assetdef = hou.hda.definitionsInFile(hda_path)[hda_definition_index]
+    asset = geo.createNode(assetdef.nodeTypeName())
+
+    # Set parms
+    apply_params(asset, params)
 
     # Set inputs
     for i, input_file in enumerate(parms_data['inputs']):
@@ -138,13 +131,56 @@ def export_mesh(request: ExportMeshRequest, result_callback):
         usdz_node.parm("execute").pressButton()
         os.remove(output_file_path)
 
-    hou.hda.uninstallFile(hdapath)
+    hou.hda.uninstallFile(hda_path)
 
     mdarol.end_houdini(hip)
     mdarol.remove_file(hip)
 
+
+def upload_mesh(token: str, file_path: str) -> str:
+    headers = {"Authorization": "Bearer %s" % token}
+
+    file_id = None
+    with open(file_path, 'rb') as file:
+        file_name = os.path.basename(file_path)
+        file_data = [
+            ('files', (file_name, file, 'application/octet-stream'))]
+        response = requests.post(
+            f"{ENDPOINT}/upload/store",
+            headers=headers, files=file_data, timeout=10)
+        if response.status_code == 200:
+            result = response.json()
+            file_id = result['files'][0].file_id
+
+    return file_id
+
+
+class ExportMeshRequest(BaseModel):
+    hda_file: FileParameter
+    hda_definition_index: int
+    format: str
+
+
+def export_mesh(request: ParameterSet, result_callback):
+    model = ExportMeshRequest(**request.params)
+
+    result_file_id = None
+
+    with tempfile.TemporaryDirectory() as tmp_dir:
+        out_file_path = os.path.join(tmp_dir, "result_mesh.usdz")
+
+        generate_mesh(
+            model.hda_file.file_path,
+            model.hda_definition_index,
+            model.format,
+            request.params,
+            out_file_path,
+            tmp_dir
+        )
+
+        result_file_id = upload_mesh(out_file_path)
+
     result_callback({
-        'message': f"Mesh generation completed. Output -> {os.path.join(request.output_path,request.output_file_name)}",
-        'debug': f"Consider changing the way files are specified in an out given the new interface",
+        'message': f"Mesh generation completed. Uploaded {result_file_id}.",
         'houdini_version': f"{hou.applicationName()} - {hou.applicationVersionString()}"
     })
