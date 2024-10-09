9d179fabd3a53a7cd2ba796edd838d1e287d2f3f
pedromartello <pedro@mythica.ai>
2024-10-08 23:28:20 -0400

    [automation] Refactoring asyncio use

--- libs/python/ripple/ripple/automation.py
+++ libs/python/ripple/ripple/automation.py
@@ -12,28 +12,14 @@ from ripple.runtime.params import resolve_params
 
 # Set up logging
 logging.basicConfig(
-    level=logging.INFO,
+    level=logging.DEBUG,
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 log = logging.getLogger(__name__)
 
 NATS_URL= 'nats://nats.nats:4222'
 API_URL= 'https://api.mythica.ai/v1'
 
-
-class AsyncAdapter():
-    def schedule_task(self, task_def):
-        """Houdini has an event loop already running so we need to work around it"""
-        loop = None
-        try:
-            loop = asyncio.get_running_loop()
-            log.info("Take existing event loop: %s", loop)
-        except RuntimeError:
-            loop = asyncio.new_event_loop()
-            log.info("Created new event loop: %s", loop)
-
-        asyncio.run_coroutine_threadsafe(task_def(),loop)
-    
-class NatsAdapter(AsyncAdapter):
+class NatsAdapter():
     def __init__(self, nats_url=NATS_URL) -> None:
         self.nats_url = nats_url
         self.listeners = {}
@@ -55,92 +41,82 @@ class NatsAdapter(AsyncAdapter):
             self.nc = None
             log.debug("Disconnected from NATS")
 
-    def post(self, subject: str, data: dict) -> None:
+    async def post(self, subject: str, data: dict) -> None:
         """Post data to NATS on subject. """
-        async def _post_async() -> None:
-            """Post data to NATS on subject. """
-            await self._connect()                
+        await self._connect()                
+        try:
+            log.debug(f"Sending to NATS: {subject} - {data}" )
+            asyncio.create_task(self.nc.publish(subject, json.dumps(data).encode()))
+            log.debug(f"Sent to NATS")
+        except Exception as e:
+            log.error(f"Sending to NATS failed: {subject} - {data} - {e}")
+        finally:
+            if not self.listeners:
+                await self._disconnect()
+
+
+    async def listen(self, subject: str, callback: callable):
+        """ Listen to NATS """
+        await self._connect()
+
+        if subject in self.listeners:
+            log.warning(f"NATS listener already active for subject {subject}")
+            return
+
+        async def message_handler(msg):
             try:
-                log.debug(f"Sending to NATS: {subject} - {data}" )
-                await self.nc.publish(subject, json.dumps(data).encode())
-                log.debug(f"Sent to NATS")
+                payload = json.loads(msg.data.decode('utf-8'))
+                log.info(f"Received message on {subject}: {payload}")
+                asyncio.create_task(callback(payload))
             except Exception as e:
-                log.error(f"Sending to NATS failed: {subject} - {data} - {e}")
-            finally:
-                if not self.listeners:
-                    await self._disconnect()
+                log.error(f"Error processing message on {subject}: {e}")
 
-        self.schedule_task(_post_async)
+        try:
+        
+            # Wait for the response with a timeout (customize as necessary)
+            log.info("Setting up NATS response listener")
+            listener = await self.nc.subscribe(subject, cb=message_handler)
+            self.listeners[subject] = listener
+            log.info("NATS response listener set up")
 
-    # Synchronous wrapper for submitting work to NATS
-    def listen(self, subject: str, callback: callable):
-    
-        async def _listen_async() -> None:
-            await self._connect()
+        except Exception as e:
+            log.error(f"Error setting up listener for subject {subject}: {e}")
+            raise e
 
-            if subject in self.listeners:
-                log.warning(f"NATS listener already active for subject {subject}")
-                return
+    async def unlisten(self, subject: str):
 
-            async def message_handler(msg):
-                try:
-                    payload = json.loads(msg.data.decode('utf-8'))
-                    log.info(f"Received message on {subject}: {payload}")
-                    await callback(payload)
-                except Exception as e:
-                    log.error(f"Error processing message on {subject}: {e}")
-
-            try:
-            
-                # Wait for the response with a timeout (customize as necessary)
-                log.info("Setting up NATS response listener")
-                listener = await self.nc.subscribe(subject, cb=message_handler)
-                self.listeners[subject] = listener
-                log.info("NATS response listener set up")
-
-            except Exception as e:
-                log.error(f"Error setting up listener for subject {subject}: {e}")
-                raise e
-        self.schedule_task(_listen_async)
-
-    def unlisten(self, subject: str):
-        async def _unlisten_async()-> None:
-
-            """Shut down the listener for a specific subject."""
-            if subject in self.listeners:
-                log.info(f"Shutting down listener for subject {subject}")
-                subscription = self.listeners.pop(subject)
-                await subscription.unsubscribe()
-                log.info(f"Listener for subject {subject} shut down")
-                if not self.listeners:
-                    await self._disconnect()
-                    log.info(f"Last Listener was shut down. Closing Connection")
-            else:
-                log.warning(f"No active listener found for subject {subject}")
-        self.schedule_task(_unlisten_async)
+        """Shut down the listener for a specific subject."""
+        if subject in self.listeners:
+            log.info(f"Shutting down listener for subject {subject}")
+            subscription = self.listeners.pop(subject)
+            await subscription.unsubscribe()
+            log.info(f"Listener for subject {subject} shut down")
+            if not self.listeners:
+                await self._disconnect()
+                log.info(f"Last Listener was shut down. Closing Connection")
+        else:
+            log.warning(f"No active listener found for subject {subject}")
 
 
-class RestAdapter(AsyncAdapter):
+class RestAdapter():
     def __init__(self, api_url=API_URL) -> None:
         self.api_url = api_url
 
     
-    def post(self, endpoint: str, data: dict={}) -> None:
+    async def post(self, endpoint: str, data: dict={}) -> None:
         """Post data to an endpoint. """
         endpoint = os.path.join(API_URL,endpoint)
-        async def _post_async() -> None:
-            async with aiohttp.ClientSession() as session:
-                log.debug(f"Sending to Endpoint: {endpoint} - {data}" )
-                async with session.post(
-                            endpoint, 
-                            json=json.dumps(data), 
-                            headers={"Content-Type": "application/json"}) as post_result:
-                    if post_result.status in [200,201]:
-                        log.debug(f"Endpoint Response: {post_result.status}")
-                    else:
-                        log.error(f"Failed to call job API: {endpoint} - {data} - {post_result.status}")
+        async with aiohttp.ClientSession() as session:
+            log.debug(f"Sending to Endpoint: {endpoint} - {data}" )
+            async with session.post(
+                        endpoint, 
+                        json=json.dumps(data), 
+                        headers={"Content-Type": "application/json"}) as post_result:
+                if post_result.status in [200,201]:
+                    log.debug(f"Endpoint Response: {post_result.status}")
+                else:
+                    log.error(f"Failed to call job API: {endpoint} - {data} - {post_result.status}")
 
-        self.schedule_task(_post_async)
 
 
 class WorkerModel(BaseModel):
@@ -165,22 +141,17 @@ class Worker:
         self.workers:dict[str,WorkerModel] = {}
         self.nats = NatsAdapter()
         self.rest = RestAdapter()
-
+        
+        
     def start(self,subject: str, workers:list[dict]) -> None:
         self._load_workers(workers)
-        self.nats.listen(subject, self._execute())
-        def _wait():
-            """Keep the event loop running forever to wait for messages."""
-            log.info("Waiting for messages...")
-            loop = asyncio.get_event_loop()
-            try:
-                # Ensure the event loop keeps running
-                loop.run_forever()
-            except KeyboardInterrupt:
-                log.info("Received exit signal. Shutting down...")
-                loop.stop()
-
-        _wait()
+        loop = asyncio.get_event_loop()
+        loop.create_task(self.nats.listen(subject, self._get_executor()))
+        try:
+            loop.run_forever()
+        except KeyboardInterrupt:
+            log.info("Received exit signal. Shutting down...")
+            loop.stop()
 
     def _load_workers(self,workers):
         """Function to dynamically discover and register workers in a container"""
@@ -195,8 +166,6 @@ class Worker:
             log.error(f'Failed to register workers: {e}')
  
 
-
-
     #Callback for reporting back. 
     def _result(self,rdata: dict={}, complete=False):
         JOB_RESULT_ENDPOINT="/jobs/results/"
@@ -205,17 +174,17 @@ class Worker:
         log.info(f"Job {'Result' if not complete else 'Complete'} -> {rdata}")
 
         #TODO: We need to unresolve file params in the result_data field and externalize the "result" channel
-        self.nats.post("result", json.dumps(rdata)) 
+        asyncio.create_task(self.nats.post("result", json.dumps(rdata))) 
         if 'job_id' in rdata:
             job_id = rdata['job_id']
             if complete:
-                self.rest.post(f"{JOB_COMPLETE_ENDPOINT}/{job_id}")
+                asyncio.create_task(self.rest.post(f"{JOB_COMPLETE_ENDPOINT}/{job_id}"))
             else:
-                self.rest.post(f"{JOB_RESULT_ENDPOINT}/{job_id}", rdata)
+                asyncio.create_task(self.rest.post(f"{JOB_RESULT_ENDPOINT}/{job_id}", rdata))
             
 
 
-    def _execute(self):
+    def _get_executor(self):
         """
         Execute a work unit by trying to find a route that maps
         to the path defined in the payload. If a route is defined,
@@ -235,7 +204,7 @@ class Worker:
 
             except Exception as e:
                 msg=f'Error parsing payload - {json_payload} - {str(e)}'
-                doer.nats.post("result", {'status': msg })
+                await doer.nats.post("result", {'status': msg })
                 #Cannot report to endpoint as message could not be parsed
                 log.error(f"Validation error: {msg}")
                 return 
