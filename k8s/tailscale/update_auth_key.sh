0614dd287247bd6f7c173b9f49898241b3ed2064
pedromartello <pedro@mythica.ai>
2024-10-10 17:18:20 -0400

    [nats,tailscale] adding nats and tailscale K8s install recipes

---
+++ k8s/tailscale/update_auth_key.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Prompt for the auth key
+read -p "Enter the Tailscale AUTH_KEY: " AUTH_KEY
+
+# Generate the YAML and apply it to Kubernetes
+cat <<EOF | kubectl apply -f-
+apiVersion: v1
+kind: Secret
+metadata:
+  name: tailscale-auth
+stringData:
+  TS_AUTHKEY: ${AUTH_KEY}
+EOF
+
