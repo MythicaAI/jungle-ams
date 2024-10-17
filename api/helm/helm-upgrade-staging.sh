7a71520935a7247eab358dd6dea75bce0ec7c5f8
bohdan-python-dev <greenboda@gmail.com>
2024-10-17 15:20:46 +0300

    ci(deployment): Add git jobs to build and deploy api/app. Resolve alembic versions. Add restart pods after deploying.

--- api/helm/helm-upgrade-staging.sh
+++ api/helm/helm-upgrade-staging.sh
@@ -1,2 +1,8 @@
 #!/usr/bin/env bash
 helm upgrade --namespace api-staging -f api/values-staging.yaml api-staging ./api
+
+kubectl rollout restart deployment/app -n api-staging
+kubectl rollout restart deployment/gcs-proxy -n api-staging
+kubectl rollout restart deployment/lets-encrypt -n api-staging
+kubectl rollout restart deployment/packager -n api-staging
+kubectl rollout restart deployment/web-front -n api-staging
