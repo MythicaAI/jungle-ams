38437eadbac6a1d8b1a6ba5e60d8507924df5bef
Jacob Repp <jacobrepp@gmail.com>
2024-11-13 09:32:58 -0800

    [api] invalid auth token testing

--- api/app/auth/data.py
+++ api/app/auth/data.py
@@ -18,7 +18,7 @@ def get_bearer_token(authorization: str) -> str:
     auth_parts = authorization.split(' ')
     if len(auth_parts) != 2 or not auth_parts[0] == 'Bearer':
         raise HTTPException(
-            HTTPStatus.BAD_REQUEST, detail=f'Invalid Authorization header "{authorization}"')
+            HTTPStatus.BAD_REQUEST, detail=f'Authorization must be a Bearer token"')
     return auth_parts[1]
 
 
