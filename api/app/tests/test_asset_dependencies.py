14c90cd080f78b9bdf2ed5a2d89a232e93475928
Jacob Repp <jacobrepp@gmail.com>
2024-11-15 10:20:47 -0800

    tests passing checkpoint

--- api/app/tests/test_asset_dependencies.py
+++ api/app/tests/test_asset_dependencies.py
@@ -10,10 +10,12 @@ from tests.shared_test import assert_status_code, make_random_content
 
 # pylint: disable=redefined-outer-name, unused-import
 def version_id_as_str(version):
+    """Convert version tuple/list to string"""
     return '.'.join(map(str, version))
 
 
 def dependencies_uri(api_base, asset):
+    """Get the dependencies URI for an asset version"""
     return (f"{api_base}/assets/{asset.asset_id}/versions/"
             f"{version_id_as_str(asset.version)}/dependencies")
 
@@ -65,20 +67,43 @@ def test_asset_dependencies(client, api_base, create_profile, create_asset, uplo
     assert len(result.missing) == 0
     assert len(result.dependencies) == 2
     assert len(result.packages) == 2
-    assert result.packages[0].file_id == base_asset.package_id
-    assert result.packages[1].file_id == dependent_asset.package_id
-    assert result.dependencies[0].asset_id == base_asset.asset_id
-    assert result.dependencies[1].asset_id == dependent_asset.asset_id
-    assert result.dependencies[1].version == dependent_asset.version
+    # the results are from the dependent asset to it's base
+    assert result.packages[0].file_id == dependent_asset.package_id
+    assert result.packages[1].file_id == base_asset.package_id
+    assert result.dependencies[0].asset_id == dependent_asset.asset_id
+    assert result.dependencies[1].asset_id == base_asset.asset_id
+    assert result.dependencies[1].version == base_asset.version
     assert result.dependencies[1].author_name == test_profile.profile.name
 
     # create a new asset with a different profile
     other_profile = create_profile()
 
     # initially this asset has no dependencies
-    versioned_asset = create_asset(other_profile, uploader)
+    versioned_asset = create_asset(other_profile, uploader, attach_package=False)
 
     # query dependencies
+    r = client.get(
+        dependencies_uri(api_base, versioned_asset),
+        headers=test_profile.authorization_header(),
+    )
+    assert_status_code(r, HTTPStatus.OK)
+    result = AssetDependencyResult(**r.json())
+    assert len(result.missing) == 1
+    assert len(result.dependencies) == 0
+    assert len(result.packages) == 0
+    assert result.missing[0].missing_package_link == True
+    assert result.missing[0].missing_version == (versioned_asset.asset_id, (1, 0, 0))
+
+    # attach package to version 1.0.0
+    package = make_random_content("zip")
+    package_response_files = uploader(
+        other_profile.profile.profile_id,
+        other_profile.authorization_header(),
+        [package],
+        storage_uri=f"/upload/package/{versioned_asset.asset_id}/1.0.0")
+    assert len(package_response_files) == 1
+
+    # query dependencies with attached package
     r = client.get(
         dependencies_uri(api_base, versioned_asset),
         headers=test_profile.authorization_header(),
@@ -89,11 +114,16 @@ def test_asset_dependencies(client, api_base, create_profile, create_asset, uplo
     assert len(result.dependencies) == 1
     assert len(result.packages) == 1
     assert result.dependencies[0].asset_id == versioned_asset.asset_id
+    assert result.dependencies[0].version == [1, 0, 0]
 
-    # create a new version
+    # create a new version with the dependency added
+    #
+    # after this update the chain will be
+    # versioned_asset-1.0.1 -> dependant-1.0.0 -> base-1.0.0
+    #
     contents = dict(versioned_asset.contents)
 
-    # translate back to core JSON
+    # translate back to core JSON, add the dependency
     contents['files'] = [x.model_dump() for x in contents['files']]
     contents['thumbnails'] = [x.model_dump() for x in contents['thumbnails']]
     contents['dependencies'] = [
@@ -108,7 +138,7 @@ def test_asset_dependencies(client, api_base, create_profile, create_asset, uplo
         'commit_ref': versioned_asset.commit_ref + '-updated'
     }
 
-    # update the version with a dependency
+    # post the update
     new_version = list(versioned_asset.version)
     new_version[2] += 1
     new_version_str = '.'.join(map(str, new_version))
@@ -119,16 +149,26 @@ def test_asset_dependencies(client, api_base, create_profile, create_asset, uplo
     )
     assert_status_code(r, HTTPStatus.CREATED)
 
+    # query dependencies to verify the missing package link on the updated version
+    versioned_asset.version = new_version
+    r = client.get(
+        dependencies_uri(api_base, versioned_asset),
+    )
+    result = AssetDependencyResult(**r.json())
+    assert len(result.missing) == 1
+    assert result.missing[0].missing_package_link == True
+    assert result.missing[0].missing_version == (versioned_asset.asset_id, (1, 0, 1))
+
     # attach package
     package = make_random_content("zip")
     package_response_files = uploader(
-        test_profile.profile.profile_id,
+        other_profile.profile.profile_id,
         other_profile.authorization_header(),
         [package],
         storage_uri=f"/upload/package/{versioned_asset.asset_id}/{new_version_str}")
     assert len(package_response_files) == 1
 
-    # query dependencies
+    # query dependencies now with attached package
     r = client.get(
         f"{api_base}/assets/{versioned_asset.asset_id}/versions/{new_version_str}/dependencies",
         headers=test_profile.authorization_header(),
@@ -139,7 +179,7 @@ def test_asset_dependencies(client, api_base, create_profile, create_asset, uplo
     assert len(result.dependencies) == 3
     assert len(result.packages) == 3
 
-    # query dependencies for previous version
+    # query dependencies for previous version, only dependent on self
     r = client.get(
         f"{api_base}/assets/{versioned_asset.asset_id}/versions/1.0.0/dependencies",
         headers=test_profile.authorization_header(),
@@ -147,8 +187,8 @@ def test_asset_dependencies(client, api_base, create_profile, create_asset, uplo
     assert_status_code(r, HTTPStatus.OK)
     result = AssetDependencyResult(**r.json())
     assert len(result.missing) == 0
-    assert len(result.dependencies) == 0
-    assert len(result.packages) == 0
+    assert len(result.dependencies) == 1
+    assert len(result.packages) == 1
 
     # unpublish the dependency, validate that it is missing
     r = client.post(
@@ -158,28 +198,11 @@ def test_asset_dependencies(client, api_base, create_profile, create_asset, uplo
     )
     assert_status_code(r, HTTPStatus.OK)
 
-    # query dependencies again
+    # query dependencies again, should be missing the version
     versioned_asset.version = [1, 0, 1]
     r = client.get(
         dependencies_uri(api_base, versioned_asset),
     )
     result = AssetDependencyResult(**r.json())
     assert len(result.missing) == 1
-    assert result.missing[0].missing_version == (dependent_asset.asset_id, (1, 0, 0))
-
-    # publish the dependency, remove the package
-    r = client.post(
-        f"{api_base}/assets/{dependent_asset.asset_id}/versions/{version_id_as_str(dependent_asset.version)}",
-        json={"published": True, "package_id": None},
-        headers=test_profile.authorization_header(),
-    )
-    assert_status_code(r, HTTPStatus.OK)
-
-    # query dependencies again
-    r = client.get(
-        dependencies_uri(api_base, versioned_asset),
-    )
-    result = AssetDependencyResult(**r.json())
-    assert len(result.missing) == 1
-    assert result.missing[0].missing_package_link == True
     assert result.missing[0].missing_version == (dependent_asset.asset_id, (1, 0, 0))
