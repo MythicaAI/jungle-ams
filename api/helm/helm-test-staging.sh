c342a1efdabb8d467e65d4f2c7fd197f53866076
Jacob Repp <jacobrepp@gmail.com>
2024-11-16 21:21:50 -0800

    [gha] update image deployment automation
    - fix image names
    - add image name helper to helm

--- api/helm/helm-test-staging.sh
+++ api/helm/helm-test-staging.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/env bash
-helm template --namespace api-staging --debug ./api
+helm template --namespace api-staging \
+    -f api/values-staging.yaml \
+    -f api/values-images.yaml \
+    --debug ./api
