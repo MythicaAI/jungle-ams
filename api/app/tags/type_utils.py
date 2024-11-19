a9a19098e243c8151cc29e1869a6cd5ef10717e2
Local Test Script <local@test.com>
2024-11-19 14:13:59 +0200

    feat([api/app/tags]): Add tag_ids to type-models responses. Add tests.

--- api/app/tags/type_utils.py
+++ api/app/tags/type_utils.py
@@ -1,3 +1,5 @@
+from typing import Union
+from cryptid.cryptid import tag_seq_to_id
 from sqlmodel import Session, select
 
 
@@ -7,7 +9,7 @@ from tags.tag_models import get_model_type, TagType
 
 def resolve_type_tags(
     session: Session, tag_type: TagType, type_seq: int
-) -> list[(int, str)]:
+) -> list[dict[str, Union[str, int]]]:
     """Resolve all the tags for a model_type"""
 
     model_type = get_model_type(tag_type)
@@ -17,5 +19,8 @@ def resolve_type_tags(
         .where(model_type.type_seq == type_seq)
         .outerjoin(Tag, model_type.tag_seq == Tag.tag_seq)
     ).all()
-    converted = [r[1].name for r in tag_results]
+    converted = [
+        {"tag_name": r[1].name, "tag_id": tag_seq_to_id(r[1].tag_seq)}
+        for r in tag_results
+    ]
     return converted
