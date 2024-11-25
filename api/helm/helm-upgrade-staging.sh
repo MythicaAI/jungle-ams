9f02ce263fd3c51bd352598d1d00613ed1a085ac
Jacob Repp <jacobrepp@gmail.com>
2024-11-25 12:07:22 -0800

    [chore] update helm for prod (#443)
    * update image gen script name
    
    * update nginx gke config
    
    * update helm for prod

--- api/helm/helm-upgrade-staging.sh
+++ api/helm/helm-upgrade-staging.sh
@@ -1,28 +1,14 @@
 #!/usr/bin/env bash
 
-# Define version paths
-VERSION_APP_PATH="api/app/VERSION_APP"
-VERSION_FRONT_PATH="sites/VERSION_FRONT"
-
-# Read the versions from the files
-APP_VERSION=$(cat "$VERSION_APP_PATH")
-FRONT_VERSION=$(cat "$VERSION_FRONT_PATH")
-
-if [[ -f "$VERSION_APP_PATH" && -f "$VERSION_FRONT_PATH" ]]; then
-    APP_VERSION=$(cat "$VERSION_APP_PATH")
-    FRONT_VERSION=$(cat "$VERSION_FRONT_PATH")
-else
-    echo "Version files not found!"
-    exit 1
-fi
-
 # Upgrade the Helm chart with version information in the description
-helm upgrade --namespace api-staging -f api/helm/api/values-staging.yaml api-staging api/helm/api \
-  --description "Versions: back: $APP_VERSION, web-front: $FRONT_VERSION" || {
+helm upgrade --namespace api-staging -f api/helm/api/values-staging.yaml -f api/helm/api/values-images.yaml api-staging api/helm/api || {
     echo "Helm upgrade failed"
     exit 1
 }
 
 kubectl rollout restart deployment/app -n api-staging
 kubectl rollout restart deployment/packager -n api-staging
+kubectl rollout restart deployment/lets-encrypt -n api-staging
+kubectl rollout restart deployment/gcs-proxy -n api-staging
+kubectl rollout restart deployment/canary -n api-staging
 kubectl rollout restart deployment/web-front -n api-staging
