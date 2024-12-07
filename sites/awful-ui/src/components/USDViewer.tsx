157776e98b7074ba6a3c7fe48700bf7135ba82c8
Pedro Martello <pedro@mythica.ai>
2024-12-07 17:30:27 -0500

    [sites/awful-ui] Bug fix in USDVIewer (#522)
    Work around issue when zoomed out and getBoundingClientRect() returns incorrect size

--- sites/awful-ui/src/components/USDViewer.tsx
+++ sites/awful-ui/src/components/USDViewer.tsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useViewport } from '@xyflow/react';
+
+type methodType = () => { width: number, height: number, top: number, left: number, right: number, bottom: number }
+type USDViewerType = React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> 
+                          & { 
+                            src: string; 
+                            alt?: string, 
+                            subs?: boolean,
+                            oldMethod?: methodType;
+                            getBoundingClientRect?: methodType;
+                          }
 
 declare global {
   // eslint-disable-next-line @typescript-eslint/no-namespace
   namespace JSX {
+    
     interface IntrinsicElements {
-      'usd-viewer': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> 
-                    & { src: string; alt?: string, width: string, height: string };
+      'usd-viewer': USDViewerType
     }
   }
 }
@@ -17,15 +28,37 @@ interface USDViewerProps {
 }
 
 const USDViewer: React.FC<USDViewerProps> = ({ src, alt, style }) => {
+  const viewerRef = React.useRef<USDViewerType>(null);
+  const viewport = useViewport();
+  useEffect(() => {
+    if (viewerRef.current) {
+      
+      viewerRef.current.oldMethod = viewerRef.current.getBoundingClientRect as methodType;
+      const curBox = viewerRef.current.oldMethod(); 
+      if (!viewerRef.current.subs) {
+        curBox.width = curBox.width / viewport.zoom;
+        curBox.height = curBox.height / viewport.zoom;
+      }
+      viewerRef.current.getBoundingClientRect = function() {
+        return new DOMRect(
+          0,
+          0, 
+          curBox.width, 
+          curBox.height);
+      };
+      viewerRef.current.subs = true
+    }
+
+  }, [viewport.zoom]);
 
   return (
     <div style={style}>
       <usd-viewer
+        ref={viewerRef}
         src={src}
         alt={alt}
         width="100%"
         height="100%"
-        
       ></usd-viewer>
     </div>
   );
