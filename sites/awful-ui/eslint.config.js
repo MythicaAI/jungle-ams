1fc3beae60d654050edc8f6192280b44cadbaf55
Pedro Martello <pedro@mythica.ai>
2024-12-24 12:23:54 -0500

    [awful-ui] consolidate lint config (#589)

--- sites/awful-ui/eslint.config.js
+++ sites/awful-ui/eslint.config.js
@@ -1,21 +1,44 @@
+// eslint.config.js
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
-
+import pluginReactHooks from "eslint-plugin-react-hooks";
+import pluginReactRefresh from "eslint-plugin-react-refresh";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
   {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    ignores: ["dist/**"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+      parser: tseslint.parser,
+      parserOptions: {
+        ecmaVersion: 2020,
+      },
+    },
+    plugins: {
+      "react-refresh": pluginReactRefresh,
+      "react-hooks": pluginReactHooks,
+    },
+    rules: {
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      '@typescript-eslint/ban-ts-comment': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+    },
     settings: {
       react: {
         version: "detect",
       }
     }
   },
-   pluginReact.configs.flat.recommended,
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
 ];
