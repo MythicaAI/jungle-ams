44e6460947313c89009df1f7ee159ac53ed5b08f
Kyrylo <117627600+KatkovKyrylo@users.noreply.github.com>
2025-01-11 01:31:10 +0200

    [awful-ui] Feat: Copy & Paste V1 (#606)
    * [awful-ui] Feat: Copy & Paste V1
    
    * [awful-ui] Fix lint

--- sites/awful-ui/src/AwfulUI.tsx
+++ sites/awful-ui/src/AwfulUI.tsx
@@ -1,6 +1,6 @@
 // MythicaFlow.tsx
 import React, { useRef, useMemo, useState } from 'react';
-import { ReactFlow, MiniMap, Controls, Background } from '@xyflow/react';
+import { ReactFlow, MiniMap, Controls, Background, Panel } from '@xyflow/react';
 import '@xyflow/react/dist/style.css';
 
 import useAwfulFlow from './hooks/useAwfulFlow';
@@ -10,15 +10,17 @@ import AutomationNode from './components/nodes/AutomationNode';
 import FileUploadNode from './components/nodes/FileUploadNode';
 import FileViewerNode from './components/nodes/FileViewerNode';
 import HDANode from './components/nodes/HDANode';
-import { Stack } from '@mui/joy';
+import { Button, Stack } from '@mui/joy';
 import { Header } from './components/Header';
 import { TabValues } from './enums';
 import { FileEdge } from './components/edges/FileEdge';
+import useCopyPaste from './hooks/useCopyPaste';
 
 // Main Awful UI component
 const AwfulUI: React.FC = () => {
   const [tab, setTab] = useState<string>(TabValues.EDIT);
   const reactFlowWrapper = useRef<HTMLDivElement | null>(null);
+  const { cut, copy, paste, bufferedNodes } = useCopyPaste();
   const {
     onConnect,
     onDisconnect,
@@ -31,6 +33,8 @@ const AwfulUI: React.FC = () => {
     onEdgesChange,
     setRfInstance,
   } = useAwfulFlow();
+  const canCopy = nodes.some(({ selected }) => selected);
+  const canPaste = bufferedNodes.length > 0;
 
   const nodeTypes = useMemo(
     () => ({
@@ -76,6 +80,32 @@ const AwfulUI: React.FC = () => {
             <MiniMap zoomable pannable />
             <Controls />
             <Background />
+            <Panel position="top-left">
+              <Button
+                onClick={() => cut()}
+                disabled={!canCopy}
+                color="danger"
+                sx={{ mr: '6px', py: '5px', minHeight: 'auto' }}
+              >
+                Cut
+              </Button>
+              <Button
+                onClick={() => copy()}
+                disabled={!canCopy}
+                color="primary"
+                sx={{ mr: '6px', py: '4px', minHeight: 'auto' }}
+              >
+                Copy
+              </Button>
+              <Button
+                onClick={() => paste({ x: 0, y: 0 })}
+                disabled={!canPaste}
+                color="neutral"
+                sx={{ py: '4px', minHeight: 'auto' }}
+              >
+                Paste
+              </Button>
+            </Panel>
           </ReactFlow>
         </div>
         <Sidebar tab={tab} />
