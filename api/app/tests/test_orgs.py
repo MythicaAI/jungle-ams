e3082b39addae08bf7cd7abf205ae8189d96d0ea
Jacob Repp <jacobrepp@gmail.com>
2025-01-22 09:50:32 -0800

    convert tests to use internal refresh_token

--- api/app/tests/test_orgs.py
+++ api/app/tests/test_orgs.py
@@ -19,7 +19,7 @@ def test_create_update(client, api_base, create_profile, create_org):
     org_id = o.org_id
 
     # refresh the auth token (with roles) after creating an org
-    test_profile.auth_token = refresh_auth_token(client, api_base, test_profile)
+    test_profile.auth_token = refresh_auth_token(test_profile)
     headers = test_profile.authorization_header()
 
     payload = {'name': 'test-updated',
@@ -53,10 +53,8 @@ def test_org_ref_operations(client, api_base, create_profile, create_org):
     admin_id = org.profile_id
 
     # refresh auth
-    admin_profile_test_info.auth_token = refresh_auth_token(
-        client, api_base, admin_profile_test_info)
-    user_profile_test_info.auth_token = refresh_auth_token(
-        client, api_base, user_profile_test_info)
+    admin_profile_test_info.auth_token = refresh_auth_token(admin_profile_test_info)
+    user_profile_test_info.auth_token = refresh_auth_token(user_profile_test_info)
     headers = admin_profile_test_info.authorization_header()
 
     # validate that unknown roles fail
