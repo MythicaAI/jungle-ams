9aaa12916900a7e5b0cebb5d1d40a8ee69f7b56f
Pedro Martello <pedro@mythica.ai>
2025-01-24 11:26:09 -0500

    [env/dev] fully enabling automation and awful on dev (docker) (#649)

--- testing/storage/minio-config/docker-entrypoint.sh
+++ testing/storage/minio-config/docker-entrypoint.sh
@@ -2,14 +2,10 @@
 #
 # Add the test access to minio, this container depends on minio
 # having started so it is ready for these commands
-mc alias set local http://${MINIO_ENDPOINT} minio minio123
-mc admin user add local ${MINIO_ACCESS_KEY} ${MINIO_SECRET_KEY}
-mc admin policy attach local readwrite --user=${MINIO_ACCESS_KEY}
-
-# create a static bucket to hold images for the web front end, including
-# user uploaded thumbnails
+mc alias set local http://${MINIO_ENDPOINT} ${MINIO_ROOT_USER} ${MINIO_ROOT_PASSWORD}
 mc mb ${MINIO_ENDPOINT}/images
-mc anonymous set download local/images
-
-# create the packages folder
 mc mb ${MINIO_ENDPOINT}/packages
+mc mb ${MINIO_ENDPOINT}/files
+mc anonymous set download local/files
+mc anonymous set download local/images
+mc admin accesskey create local --name 'Mythica Default' --access-key foo-access --secret-key bar-secret --policy policy.json
