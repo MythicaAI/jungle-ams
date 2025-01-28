61e578f82c4d01b803faa7204b68399e3aaeea81
Jacob Repp <jacobrepp@gmail.com>
2025-01-28 08:49:55 -0800

    soft reset rebase

--- libs/python/ripple/tests/test_params.py
+++ libs/python/ripple/tests/test_params.py
@@ -1,22 +1,31 @@
 # pylint: disable=redefined-outer-name, unused-import
-
-import os
+import pytest
 import tempfile
 
 from ripple.compile.rpsc import compile_interface
 from ripple.models.params import (
-    ParameterSpec, 
-    ParameterSet, 
-    IntParameterSpec, 
-    FloatParameterSpec, 
-    StringParameterSpec, 
-    BoolParameterSpec, 
+    ParameterSpec,
+    ParameterSet,
+    IntParameterSpec,
+    FloatParameterSpec,
+    ParameterSpecModel, StringParameterSpec,
+    BoolParameterSpec,
     EnumValueSpec,
     EnumParameterSpec,
-    FileParameterSpec, 
-    FileParameter
+    FileParameterSpec,
+    FileParameter,
 )
-from ripple.runtime.params import validate_params, resolve_params, repair_parameters
+from ripple.runtime.params import validate_param, validate_params, resolve_params, repair_parameters, ParamError
+
+
+class UnknownTestType:
+    def __init__(self):
+        self.value = "a type to trigger an error"
+
+
+def assert_validation(e: ParamError, contents: str):
+    assert type(e) is ParamError
+    assert contents in str(e), f"{contents} not in {str(e)}"
 
 
 def test_param_compile():
@@ -286,7 +295,7 @@ def test_param_compile():
     assert compiled.params['no_category_toggle'].category_label is None
 
 
-def test_menu_parms():
+def test_menu_params():
     data = """
     {
         "defaults": {
@@ -360,12 +369,14 @@ def test_menu_parms():
     assert compiled.params["string"].values[2].name == ""
 
 
-def test_param_validate():
+def test_param_validate_minimal():
     # Minimal test
     spec = ParameterSpec(params={})
     set = ParameterSet()
-    assert validate_params(spec, set)
+    validate_params(spec, set)
 
+
+def test_param_validation_all_types():
     # All types test
     spec = ParameterSpec(params={
         'test_int': IntParameterSpec(label='test_int', default=0),
@@ -383,64 +394,145 @@ def test_param_validate():
         test_enum="a",
         test_file=FileParameter(file_id='file_qfJSVuWRJvq5PmueFPxSjXsEcST')
     )
-    assert validate_params(spec, set)
+    validate_params(spec, set)
 
+
+def test_param_validation_count():
     # Parameter count test
     spec = ParameterSpec(params={"input0": FileParameterSpec(label="Test Input 0", default='')})
     set_good = ParameterSet(input0= FileParameter(file_id="file_qfJSVuWRJvq5PmueFPxSjXsEcST"))
     set_bad = ParameterSet()
-    assert validate_params(spec, set_good)
-    assert validate_params(spec, set_bad) == False
+    validate_params(spec, set_good)
+    with pytest.raises(ParamError) as exc_info:
+        validate_params(spec, set_bad)
+    assert_validation(exc_info.value, "param not provided")
 
+def test_param_validation_type_mismatch():
     # Parameter type test
     spec = ParameterSpec(params={'test_int': IntParameterSpec(label='test', default=0)})
-    set_good = ParameterSet(test_int= 5)
-    set_bad = ParameterSet(test_int ='bad')
-    assert validate_params(spec, set_good)
-    assert validate_params(spec, set_bad) == False
+    set_good = ParameterSet(test_int=5)
+    set_bad = ParameterSet(test_int='bad')
+    validate_params(spec, set_good)
+    with pytest.raises(ParamError) as exc_info:
+        validate_params(spec, set_bad)
+    assert_validation(exc_info.value, "did not match expected type")
 
+def test_param_validation_booleans():
+    # Bool parameter test
+    spec = ParameterSpec(params={'test_bool': BoolParameterSpec(label='test', default=True)})
+    set_good = ParameterSet(test_bool=False)
+    set_bad = ParameterSet(test_bool='bad')
+    validate_params(spec, set_good)
+    with pytest.raises(ParamError) as exc_info:
+        validate_params(spec, set_bad)
+    assert_validation(exc_info.value, "did not match expected type")
+
+def test_param_validation_strings():
+    # String parameter test
+    spec = ParameterSpec(params={'test_str': StringParameterSpec(label='test', default='a')})
+    set_good = ParameterSet(test_str='b')
+    set_bad = ParameterSet(test_str=0)
+    validate_params(spec, set_good)
+    with pytest.raises(ParamError) as exc_info:
+        validate_params(spec, set_bad)
+    assert_validation(exc_info.value, "did not match expected type")
+
+
+def test_param_validation_unknown_types():
+    unknown_spec = IntParameterSpec(label='test', default=0)
+    with pytest.raises(ParamError) as exc_info:
+        validate_param(unknown_spec, 0, None)
+    assert_validation(exc_info.value, "invalid type validation")
+
+
+def test_param_validation_arrays():
     # Parameter array test
     spec = ParameterSpec(params={'test_int': IntParameterSpec(label='test', default=[1, 2, 3])})
-    set_good = ParameterSet(test_int= [4, 5, 6])
-    set_bad = ParameterSet(test_int= [1])
-    set_bad2 = ParameterSet(test_int= ['a', 'b', 'c'])
-    assert validate_params(spec, set_good)
-    assert validate_params(spec, set_bad) == False
-    assert validate_params(spec, set_bad2) == False
+    set_good = ParameterSet(test_int=[4, 5, 6])
+    validate_params(spec, set_good)
 
+    set_bad = ParameterSet(test_int=[1])
+    with pytest.raises(ParamError) as exc_info:
+        validate_params(spec, set_bad)
+    assert_validation(exc_info.value, "length mismatch")
+
+    set_bad2 = ParameterSet(test_int=['a', 'b', 'c'])
+    with pytest.raises(ParamError) as exc_info:
+        validate_params(spec, set_bad2)
+    assert_validation(exc_info.value, "did not match expected type")
+
+    # TODO-jrepp: this currently does not fail
+    #set_bad3 = ParameterSet(test_int=0)
+    #with pytest.raises(ParamError) as exc_info:
+    #    validate_params(spec, set_bad3)
+    #assert_validation(exc_info.value, "did not match expected type")
+
+
+def test_param_validation_enums():
     # Enum test
     spec = ParameterSpec(params={'test_enum': EnumParameterSpec(label='test_enum', default='a', values=[EnumValueSpec(name='a', label='A')])})
     set_good = ParameterSet(test_enum="a")
     set_bad = ParameterSet(test_enum="b")
-    assert validate_params(spec, set_good)
-    assert validate_params(spec, set_bad) == False
+    validate_params(spec, set_good)
+    with pytest.raises(ParamError) as exc_info:
+        validate_params(spec, set_bad)
+    assert_validation(exc_info.value, "not in")
 
+def test_param_validation_constants():
     # Constant test
     spec = ParameterSpec(params={'test_int': IntParameterSpec(label='test', default=0, constant=True)})
-    set_good = ParameterSet(test_int= 0)
-    set_bad = ParameterSet(test_int= 1)
-    assert validate_params(spec, set_good)
-    assert validate_params(spec, set_bad) == False
+    set_good = ParameterSet(test_int=0)
+    set_bad = ParameterSet(test_int=1)
+    validate_params(spec, set_good)
+    with pytest.raises(ParamError) as exc_info:
+        validate_params(spec, set_bad)
+    assert_validation(exc_info.value, "constant mismatch")
 
     # Populate constants test
     spec = ParameterSpec(params={
         'test_int': IntParameterSpec(label='test_int', default=0, constant=True),
         'test_file': FileParameterSpec(label='test_file', default='file_qfJSVuWRJvq5PmueFPxSjXsEcST', constant=True),
+        'test_file_array': FileParameterSpec(label='test_file_array', default=['file_qfJSVuWRJvq5PmueFPxSjXsEcST'], constant=True),
     })
     set = ParameterSet()
-    assert validate_params(spec, set) == False
+    with pytest.raises(ParamError) as exc_info:
+        validate_params(spec, set)
+    assert_validation(exc_info.value, "param not provided")
     repair_parameters(spec, set)
-    assert validate_params(spec, set)
+    validate_params(spec, set)
 
+    # validate constant length mismatch
+    with pytest.raises(ParamError) as exc_info:
+        set_invalid_length = ParameterSet(
+            test_int=0,
+            test_file=FileParameter(file_id='file_qfJSVuWRJvq5PmueFPxSjXsEcST'),
+            test_file_array=[]
+        )
+        validate_params(spec, set_invalid_length)
+    assert_validation(exc_info.value, "length mismatch")
+
+    # validate invalid type in array param, this invalidates underlying model
+    # construction of expectedType(**)
+    with pytest.raises(TypeError):
+        set_invalid_type = ParameterSet(
+            test_int=0,
+            test_file=FileParameter(file_id='file_qfJSVuWRJvq5PmueFPxSjXsEcST'),
+            test_file_array=[UnknownTestType()])
+        validate_params(spec, set_invalid_type)
+
+
+def test_param_implicit_cast_to_float():
     # Implicit int to float cast test
     spec = ParameterSpec(params={
         'test_float': FloatParameterSpec(label='test', default=0.0),
-        'test_float_array': FloatParameterSpec(label='test', default=[0.0, 0.0, 0.0])
+        'test_float_array': FloatParameterSpec(label='test_array', default=[0.0, 0.0, 0.0])
     })
     set = ParameterSet(test_float=5, test_float_array=[1, 2, 3])
-    assert validate_params(spec, set) == False
+    with pytest.raises(ParamError) as exc_info:
+        validate_params(spec, set)
+    assert_validation(exc_info.value, "did not match expected type")
     repair_parameters(spec, set)
-    assert validate_params(spec, set)
+    validate_params(spec, set)
 
 
 def test_param_resolve():
