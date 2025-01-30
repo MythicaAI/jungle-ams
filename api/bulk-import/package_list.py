6da50b62d30f433c322bb0e8cc0277ae464afb86
Jacob Repp <jacobrepp@gmail.com>
2025-01-30 08:22:37 -0800

    use https for git access to help with automation

--- api/bulk-import/package_list.py
+++ api/bulk-import/package_list.py
@@ -2,106 +2,106 @@
 
 packages = [
     {
-        'repo': "git@github.com:kdbra/kdbra-houdini-tools.git",
+        'repo': "https://github.com/kdbra/kdbra-houdini-tools.git",
         'directory': "otls",
         'name': "KDBRA Tools",
-        'description': "Kdbra tools are intended to speed up and facilitate VFX artist's routines."
+        'description': "KDBRA tools are intended to speed up and facilitate VFX artist's routines."
     },
     {
-        'repo': "git@github.com:TrevisanGMW/gt-houdini-assets.git",
+        'repo': "https://github.com/TrevisanGMW/gt-houdini-assets.git",
         'directory': "assets",
         'name': "GT Houdini Assets",
         'description': ("These assets were created with the aim of automating,"
                         "simplifying or to be used as a learning tool.")
     },
     {
-        'repo': "git@github.com:CorvaeOboro/zenv.git",
+        'repo': "https://github.com/CorvaeOboro/zenv.git",
         'directory': "hda",
         'name': "ZENV",
         'description': "Houdini hda tools focused on procedural modeling environments."
     },
     {
-        'repo': "git@github.com:mifth/mifthtools.git",
+        'repo': "https://github.com/mifth/mifthtools.git",
         'directory': "houdini/otls",
         'name': "MiraTools",
         'description': "Modern modeling and retopology tools."
     },
     {
-        'repo': "git@github.com:NiklasRosenstein/houdini-library.git",
+        'repo': "https://github.com/NiklasRosenstein/houdini-library.git",
         'directory': "otls",
         'name': "Niklas' Houdini Library",
         'description': "A collection of digital assets, shelf tools and code snippets."
     },
     {
-        'repo': "git@github.com:LaidlawFX/LaidlawFX.git",
+        'repo': "https://github.com/LaidlawFX/LaidlawFX.git",
         'directory': "otls",
         'name': "LaidlawFX",
         'description': "A repository of tools developed in production."
     },
     {
-        'repo': "git@github.com:eglaubauf/egLib.git",
+        'repo': "https://github.com/eglaubauf/egLib.git",
         'directory': "otls",
         'name': "egLib",
         'description': "A collection of scripts for SideFx Houdini."
     },
     {
-        'repo': "git@github.com:igor-elovikov/hipie.git",
+        'repo': "https://github.com/igor-elovikov/hipie.git",
         'directory': "otls",
         'name': "Houdini Tools by Igor Elovikov",
         'description': ""
     },
     {
-        'repo': "git@github.com:thi-ng/houdini.git",
+        'repo': "https://github.com/thi-ng/houdini.git",
         'directory': "hda",
         'name': "thi.ng Houdini Tools",
         'description': "Houdini HDAs & sketches (VEX, OpenCL, Python)"
     },
     {
-        'repo': "git@github.com:ivantitov-zd/Hammer.git",
+        'repo': "https://github.com/ivantitov-zd/Hammer.git",
         'directory': "otls",
         'name': "Hammer Tools",
         'description': "Hammer Tools"
     },
     {
-        'repo': "git@github.com:sashaouellet/SDMTools.git",
+        'repo': "https://github.com/sashaouellet/SDMTools.git",
         'directory': "houdini/otls",
         'name': "SDMTools",
         'description': "A collection of shelf tools, HDAs, and menu scripts in its Houdini form."
     },
     {
-        'repo': "git@github.com:captainhammy/Houdini-Toolbox.git",
+        'repo': "https://github.com/captainhammy/Houdini-Toolbox.git",
         'directory': "houdini/otls",
         'name': "Houdini Toolbox",
         'description': "Collection of Houdini tools."
     },
     {
-        'repo': "git@github.com:qLab/qLib.git",
+        'repo': "https://github.com/qLab/qLib.git",
         'directory': "otls",
         'name': "qLib",
         'description': "A procedural asset library for SideFX Houdini."
     },
     {
-        'repo': "git@github.com:joshuazt/JZTREES.git",
+        'repo': "https://github.com/joshuazt/JZTREES.git",
         'directory': "otls",
         'name': "JZTREES",
         'description': ("Set of tools designed to ease the workflow for generating"
                         "and applying FX to trees and vegetation.")
     },
     {
-        'repo': "git@github.com:demiaster/treegen.git",
+        'repo': "https://github.com/demiaster/treegen.git",
         'directory': "assets",
         'name': "Treegen",
         'description': "Vegetation Generation Tool for Houdini."
     },
     {
-        'repo': "git@github.com:CorvaeOboro/gen_item.git",
+        'repo': "https://github.com/CorvaeOboro/gen_item.git",
         'directory': "houdini",
         'name': "Gen_Item",
         'description': ("Create item images, a circular workflow of refinement"
                         "using procgen augmented by neural networks.")
     },
     {
-        'repo': "git@github.com:david-rhodes/GSOPs.git",
+        'repo': "https://github.com/david-rhodes/GSOPs.git",
         'directory': "otls",
         'name': "Gaussian Splat Operators (GSOPS)",
         'description': ("Use GSOPs to import, edit, and export gaussian splat models,"
@@ -113,13 +113,13 @@ packages = [
                         " mesh splats, and conduct feature analysis.")
     },
     {
-        'repo': "git@github.com:jamesrobinsonvfx/linewriter.git",
+        'repo': "https://github.com/jamesrobinsonvfx/linewriter.git",
         'directory': "houdini19.0/otls",
         'name': "Line Writer",
         'description': "Linewriter quickly creates lines of text geometry in Houdini."
     },
     {
-        'repo': "git@github.com:aaronsmithtv/Texture-Stamp.git",
+        'repo': "https://github.com/aaronsmithtv/Texture-Stamp.git",
         'directory': "otls",
         'name': "Texture Stamp",
         'description': ("The Texture Stamp HDA for Houdini is a simple,"
@@ -127,7 +127,7 @@ packages = [
                         " an image onto a geometry texture (stamping) easier, in a procedural workflow.")
     },
     {
-        'repo': "git@github.com:probiner/DASH.git",
+        'repo': "https://github.com/probiner/DASH.git",
         'directory': "otls",
         'name': "DASH",
         'description': ("DASH is a package for SideFX Houdini that proposes small quality of"
@@ -135,13 +135,13 @@ packages = [
                         " scripts or settings.")
     },
     {
-        'repo': "git@github.com:zengchen2015/HOUDINI_HPro.git",
+        'repo': "https://github.com/zengchen2015/HOUDINI_HPro.git",
         'directory': "otls",
         'name': "Houdini HPro",
         'description': "A number of HDAs created by zengchen2015."
     },
     {
-        'repo': "git@github.com:DominikLingenover/DMNK-Tools.git",
+        'repo': "https://github.com/DominikLingenover/DMNK-Tools.git",
         'directory': "otls",
         'name': "DMNK Shelf",
         'description': ("DMNK Shelf is a set of Python Scripts and HDAs to speed up your workflow."
@@ -150,32 +150,32 @@ packages = [
                         " with all major engines except Mantra. (Arnold, Octane, Redshift, Renderman, VRay)")
     },
     {
-        'repo': "git@github.com:firehawkvfx/firehawk-houdini-tools.git",
+        'repo': "https://github.com/firehawkvfx/firehawk-houdini-tools.git",
         'directory': "hda",
         'name': "Firehawk Houdini Tools",
         'description': "A collection of useful hda's and scripts used in the Open Firehawk VFX pipeline."
     },
     {
-        'repo': "git@github.com:Ashheer/YingQ-for-Houdini.git",
+        'repo': "https://github.com/Ashheer/YingQ-for-Houdini.git",
         'directory': "otls",
         'name': "YingQ for Houdini",
         'description': "A collection of HDAs."
     },
     {
-        'repo': "git@github.com:maximcapra/MCHoudiniLabs.git",
+        'repo': "https://github.com/maximcapra/MCHoudiniLabs.git",
         'directory': "otls",
         'name': "MC Labs - Houdini Tools",
         'description': "HDAs, Scripts, Shelf Tools and Experiments for SideFX Houdini."
     },
     {
-        'repo': "git@github.com:ShoheiOkazaki/Nagamochi.git",
+        'repo': "https://github.com/ShoheiOkazaki/Nagamochi.git",
         'directory': "common/otls",
         'name': "Nagamochi",
         'description': ("Nagamochi is a library of Digital Assets, Presets, Shelves,"
                         " and Scripts for SideFX Houdini.")
     },
     {
-        'repo': "git@github.com:Njordy/nLib.git",
+        'repo': "https://github.com/Njordy/nLib.git",
         'directory': "otls",
         'name': "nLib",
         'description': ("A growing collection of digital assets what might be helpful"
@@ -183,44 +183,44 @@ packages = [
                         " what is usually very common.")
     },
     {
-        'repo': "git@github.com:Fe-Elf/FeELib-for-Houdini.git",
+        'repo': "https://github.com/Fe-Elf/FeELib-for-Houdini.git",
         'directory': "otls",
         'name': "FE-ELib for Houdini",
         'description': "A collection of HDAs."
     },
     {
-        'repo': "git@github.com:simonreeves/BeeHou.git",
+        'repo': "https://github.com/simonreeves/BeeHou.git",
         'directory': "hdas",
         'name': "BeeHou",
         'description': "Package of tools and scripts for Sidefx Houdini."
     },
     {
-        'repo': "git@github.com:Aeoll/Aelib.git",
+        'repo': "https://github.com/Aeoll/Aelib.git",
         'directory': "otls",
         'name': "Aelib",
         'description': "A library of Digital Assets, Tools and Scripts for SideFX Houdini."
     },
     {
-        'repo': "git@github.com:WeslieSison/WesLib.git",
+        'repo': "https://github.com/WeslieSison/WesLib.git",
         'directory': "WesLib/otls",
         'name': "WesLib",
         'description': "A collection of HDAs."
     },
     {
-        'repo': "git@github.com:Aeoll/ae_SVG.git",
+        'repo': "https://github.com/Aeoll/ae_SVG.git",
         'directory': "otls",
         'name': "ae_svg",
         'description': "Python-based HDAs for importing and exporting SVG files in Houdini"
     },
     {
-        'repo': "git@github.com:alexnardini/FLAM3_for_SideFX_Houdini.git",
+        'repo': "https://github.com/alexnardini/FLAM3_for_SideFX_Houdini.git",
         'directory': "otls/H20.x",
         'name': "The Fractal Flame Algorithm",
         'description': ("An implementation of the original fractal flame algorithm"
                         " inside a procedural environment.")
     },
     {
-        'repo': "git@github.com:design-automation/houdarcs.git",
+        'repo': "https://github.com/design-automation/houdarcs.git",
         'directory': "otls",
         'name': "Houdarcs",
         'description': "Houdini Architecture Assets"
