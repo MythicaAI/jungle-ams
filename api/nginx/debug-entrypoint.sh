03d2decad25b07a427d59fb54c1d7737fd729316
Pedro Martello <pedro@mythica.ai>
2025-01-30 09:48:44 -0500

    [env/debug] fix (#667)
    - prevent output of content for files on logs
    - properly reference host in nginx

--- api/nginx/debug-entrypoint.sh
+++ api/nginx/debug-entrypoint.sh
@@ -1,1 +1,1 @@
-docker run -it --rm -e MYTHICA_ENVIRONMENT=debug -p 8080:80 --network storage mythica-web-front
+docker run -it --rm -e MYTHICA_ENVIRONMENT=debug -p 8080:80 --network storage --add-host host.docker.internal:host-gateway mythica-web-front
