594ecad4faa1c29d6ae19339c4c672c35f63863a
Jacob Repp <jacobrepp@gmail.com>
2025-01-31 08:41:32 -0800

    update with latest p4 trigger info

--- github/perforce-watcher/p4_invoke_infra_webhook.py
+++ github/perforce-watcher/p4_invoke_infra_webhook.py
@@ -4,20 +4,19 @@ import sys
 
 
 def notify_github(changelist_id: str):
-    github_token = os.environ['GITHUB_TOKEN']
+    github_token = os.environ.get("GITHUB_TOKEN")
     repo_owner = "MythicaAI"
     repo_name = "infra"
 
-    url = f"https://api.github.com/repos/{repo_owner}/{repo_name}/dispatches"
+    url = (f"https://api.github.com/repos/{repo_owner}/{repo_name}"
+           "/actions/workflows/bulk-import-mythica-p4.yaml/dispatches")
 
     headers = {
         "Accept": "application/vnd.github.v3+json",
         "Authorization": f"token {github_token}",
-        "X-GitHub-Api-Version": "2022-11-28"
     }
 
     data = {
-        "event_type": "p4-commit",
         "ref": "main",
         "inputs": {
             "changelist": changelist_id
@@ -25,6 +24,8 @@ def notify_github(changelist_id: str):
     }
     print(f"posting {data} to {url}")
     response = requests.post(url, headers=headers, json=data)
+    print("### response: ")
+    print(f"{response.text}")
     response.raise_for_status()
     return response.status_code == 204
 
@@ -34,5 +35,4 @@ if __name__ == "__main__":
         changelist = sys.argv[1]
         notify_github(changelist)
     else:
-        print("No changelist ID")
-        sys.exit(1)
+        print("no changelist provided")
