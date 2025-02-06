8ad94d2dbf2a2759cfd6192c43a5037e54de02cc
Local Test Script <local@test.com>
2025-02-06 15:35:21 +0200

    refactor([telemetry]): Do not duplicate error logs.

--- api/app/exceptions.py
+++ api/app/exceptions.py
@@ -14,7 +14,6 @@ logger = logging.getLogger(__name__)
 
 async def api_id_error(_: Request, exc: IdError):
     """Automatic IdError exception handler"""
-    logger.warning("An invalid API identifier was used: %s", exc)
     return JSONResponse(
         status_code=HTTP_400_BAD_REQUEST,
         content={"detail": f"An invalid API identifier was used: {exc}"},
@@ -23,7 +22,6 @@ async def api_id_error(_: Request, exc: IdError):
 
 async def api_seq_error(_: Request, exc: SequenceError):
     """Automatic SequenceError exception handler"""
-    logger.warning("An invalid sequence was used: %s", exc)
     return JSONResponse(
         status_code=HTTP_400_BAD_REQUEST,
         content={"detail": f"An invalid sequence was used: {exc}"}
