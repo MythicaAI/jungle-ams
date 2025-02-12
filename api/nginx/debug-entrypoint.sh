ea1559acb133602d30fd8ef2a8142342843b921b
pedromartello <pedro@mythica.ai>
2025-02-11 20:16:43 -0500

    [sites/awful-ui] Package Support - adding support for viewing and loading files from packages.

--- api/nginx/debug-entrypoint.sh
+++ api/nginx/debug-entrypoint.sh
@@ -1,1 +1,1 @@
-docker run -it --rm -e CONF_FILE=nginx-debug.conf -p 8080:80 --network storage --add-host host.docker.internal:host-gateway mythica-web-front
+docker run -it --rm -e CONF_FILE=nginx-debug.conf -p 8080:80 --network storage --add-host host.docker.internal:host-gateway --name nginx mythica-web-front
