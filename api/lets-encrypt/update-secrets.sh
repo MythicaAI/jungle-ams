b974f230c4f85cf9551851ed8c1478f8000e8272
Jacob Repp <jacobrepp@gmail.com>
2025-02-24 07:59:51 -0800

    [lets-encrypt, fix] delete the secret first before create (#756)

--- api/lets-encrypt/update-secrets.sh
+++ api/lets-encrypt/update-secrets.sh
@@ -7,8 +7,10 @@ TLS_KEY=$(cat "$RENEWED_LINEAGE/privkey.pem")
 # Log the renewed domains (optional)
 echo "Renewed domains: $RENEWED_DOMAINS"
 
+kubectl delete secrets/$TLS_NAME \
+  --namespace $TLS_NAMESPACE
+
 kubectl create secret tls $TLS_NAME \
   --cert=<(echo "$TLS_CERT") \
   --key=<(echo "$TLS_KEY") \
-  --namespace $TLS_NAMESPACE \
-  --dry-run=client -o yaml | kubectl apply -f -
+  --namespace $TLS_NAMESPACE
