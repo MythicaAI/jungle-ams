8b5e7eaebcff44ba0baaf4a7809d5b719e6409b4
kevin <kevin@mythica.ai>
2025-02-25 15:53:59 -0800

    Move parsing out to separate file

--- automation/houdini_hdk/src/houdini_worker.cpp
+++ automation/houdini_hdk/src/houdini_worker.cpp
@@ -1,5 +1,6 @@
 #include "interrupt.h"
 #include "streaming.h"
+#include "types.h"
 
 #include <OP/OP_Director.h>
 #include <OP/OP_OTLLibrary.h>
@@ -20,97 +21,10 @@ usage(const char *program)
     UT_Exit::fail();
 }
 
-struct Request
-{
-    std::string op;
-    std::string hda_file;
-    int64 definition_index;
-};
-
-bool parse_request(const std::string& message, Request& request)
-{
-    UT_AutoJSONParser parser(message.c_str(), message.size());
-    
-    UT_StringHolder op, hda_file;
-    int64 definition_index = -1;
-    
-    UT_JSONParser::iterator outer_it = parser->beginMap();
-    while (!outer_it.atEnd())
-    {
-        UT_StringHolder key;
-        if (!outer_it.getKey(key))
-        {
-            return false;
-        }
-
-        if (key == "op")
-        {
-            if (!parser->parseString(op))
-            {
-                return false;
-            }
-        }
-        else if (key == "data")
-        {
-            UT_JSONParser::iterator data_it = parser->beginMap();
-            while (!data_it.atEnd())
-            {
-                UT_StringHolder data_key;
-                if (!data_it.getKey(data_key))
-                {
-                    return false;
-                }
-
-                if (data_key == "hda_path")
-                {
-                    if (!parser->parseString(hda_file))
-                    {
-                        return false;
-                    }
-                }
-                else if (data_key == "definition_index")
-                {
-                    if (!parser->parseInt(definition_index))
-                    {
-                        return false;
-                    }
-                }
-                else
-                {
-                    if (!parser->skipNextObject())
-                    {
-                        return false;
-                    }
-                }
-                ++data_it;
-            }
-        }
-        else
-        {
-            if (!parser->skipNextObject())
-            {
-                return false;
-            }
-        }
-        ++outer_it;
-    }
-
-    if (hda_file.isEmpty() || definition_index < 0)
-    {
-        std::cerr << "Missing required fields" << std::endl;
-        return false;
-    }    
-
-    request.op = op.toStdString();
-    request.hda_file = hda_file.toStdString();
-    request.definition_index = definition_index;
-    return true;
-}
-
 bool process_message(const std::string& message, MOT_Director* boss, StreamWriter& writer)
 {
     Request request;
-    if (!parse_request(message, request))
+    if (!util::parse_request(message, request))
     {
         std::cerr << "Failed to parse request" << std::endl;
         return false;
