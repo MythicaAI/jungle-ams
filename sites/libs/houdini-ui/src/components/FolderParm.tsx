9b57d9f8e6fdcf56b9f622c049b6992b75826513
Kyrylo Katkov <kyrylo.katkov@gmail.com>
2025-03-20 18:02:16 +0200

    [houdini-ui] Folder Params design improvements

--- sites/libs/houdini-ui/src/components/FolderParm.tsx
+++ sites/libs/houdini-ui/src/components/FolderParm.tsx
@@ -1,55 +1,97 @@
 import React from 'react';
-import hou,{ dictionary } from '../types/Houdini';
+import hou, { dictionary } from '../types/Houdini';
 import { ParmFactory } from './ParmFactory'; // Reuse the view controller for nested templates
+import { LucideChevronDown, LucideChevronRight } from 'lucide-react';
 
 export interface FolderParmProps {
-    template: hou.FolderParmTemplate;
-    data: dictionary;
-    onChange: (formData: dictionary) => void; // Callback for value changes
+  template: hou.FolderParmTemplate;
+  data: dictionary;
+  onChange: (formData: dictionary) => void; // Callback for value changes
 }
 
-export const FolderParm: React.FC<FolderParmProps> = ({template, data, onChange}) => {
+export const FolderParm: React.FC<FolderParmProps> = ({
+  template,
+  data,
+  onChange,
+}) => {
+  const [isOpen, setIsOpen] = React.useState(false);
 
-    // Render based on folder_type
-    if (template.folder_type === 'Tabs') {
-        return (
-            <div className={`folder-parm tabs ${template.runtime_data.isActive ? 'active' : ''}`}>
-                <div className="folder-content">
-                    {template.parm_templates.map((parmTemplate, index) => (
-                        <div key={template.name + index} className="parm-item">
-                            <ParmFactory data={data} parmTemplate={parmTemplate} onChange={onChange} />
-                        </div>
-                    ))}
-                </div>
-            </div>
-        );
-    } else if (template.folder_type === 'Collapsible') {
-        return (
-            <details className={`folder-parm collapsible`}>
-                <summary>{template.label}</summary>
-                <div className="folder-content">
-                    {template.parm_templates.map((parmTemplate, index) => (
-                        <div key={template.name + index} className="parm-item">
-                            <ParmFactory data={data} parmTemplate={parmTemplate} onChange={onChange} />
-                        </div>
-                    ))}
-                </div>
-            </details>
-        );
-    }
+  const handleToggle = (event: React.SyntheticEvent<HTMLDetailsElement>) => {
+    const target = event.target as HTMLDetailsElement;
+    setIsOpen(target.open);
+  };
 
-    // Default rendering if folder_type is not specifically handled
+  // Render based on folder_type
+  if (template.folder_type === 'Tabs') {
     return (
-        <div className={`folder-parm default ${template.runtime_data.isActive ? 'active' : ''}`}>
-            <div className="folder-content">
-                {template.parm_templates.map((parmTemplate, index) => (
-                    <div key={template.name + index} className="parm-item">
-                        <ParmFactory data={data} parmTemplate={parmTemplate} onChange={onChange} />
-                    </div>
-                ))}
+      <div
+        className={`folder-parm tabs ${
+          template.runtime_data.isActive ? 'active' : ''
+        }`}
+      >
+        <div className="folder-content">
+          {template.parm_templates.map((parmTemplate, index) => (
+            <div key={template.name + index} className="parm-item">
+              <ParmFactory
+                data={data}
+                parmTemplate={parmTemplate}
+                onChange={onChange}
+              />
             </div>
+          ))}
         </div>
+      </div>
     );
+  } else if (template.folder_type === 'Collapsible') {
+    return (
+      <details
+        className={`folder-parm collapsible`}
+        open={isOpen}
+        onToggle={handleToggle}
+      >
+        <summary className="cursor-pointer align-center justify-start gap-4">
+          {isOpen ? (
+            <LucideChevronRight size={16} />
+          ) : (
+            <LucideChevronDown size={16} />
+          )}
+          {template.label}
+        </summary>
+        <div className="folder-content">
+          {template.parm_templates.map((parmTemplate, index) => (
+            <div key={template.name + index} className="parm-item">
+              <ParmFactory
+                data={data}
+                parmTemplate={parmTemplate}
+                onChange={onChange}
+              />
+            </div>
+          ))}
+        </div>
+      </details>
+    );
+  }
+
+  // Default rendering if folder_type is not specifically handled
+  return (
+    <div
+      className={`folder-parm default ${
+        template.runtime_data.isActive ? 'active' : ''
+      }`}
+    >
+      <div className="folder-content">
+        {template.parm_templates.map((parmTemplate, index) => (
+          <div key={template.name + index} className="parm-item">
+            <ParmFactory
+              data={data}
+              parmTemplate={parmTemplate}
+              onChange={onChange}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default FolderParm;
