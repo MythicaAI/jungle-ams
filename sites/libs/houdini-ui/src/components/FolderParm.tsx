dae909ee38d3826a3131c0e5ff22f1f0c6c7fc15
Kyrylo <117627600+KatkovKyrylo@users.noreply.github.com>
2025-03-21 16:52:27 +0200

    [houdini-ui] Folder Params design improvements (#857)
    * [houdini-ui] Folder Params design improvements
    
    * [houdini-ui] reverse collapsible triangles
    
    * [houdini-ui] Use different SVGs for chevrons

--- sites/libs/houdini-ui/src/components/FolderParm.tsx
+++ sites/libs/houdini-ui/src/components/FolderParm.tsx
@@ -1,55 +1,103 @@
 import React from 'react';
-import hou,{ dictionary } from '../types/Houdini';
+import hou, { dictionary } from '../types/Houdini';
 import { ParmFactory } from './ParmFactory'; // Reuse the view controller for nested templates
+import ChevronRight from '../assets/chevron-right.svg';
+import ChevronDown from '../assets/chevron-down.svg';
 
 export interface FolderParmProps {
-    template: hou.FolderParmTemplate;
-    data: dictionary;
-    onChange: (formData: dictionary) => void; // Callback for value changes
+  template: hou.FolderParmTemplate;
+  data: dictionary;
+  onChange: (formData: dictionary) => void; // Callback for value changes
 }
 
-export const FolderParm: React.FC<FolderParmProps> = ({template, data, onChange}) => {
+export const FolderParm: React.FC<FolderParmProps> = ({
+  template,
+  data,
+  onChange,
+}) => {
+  const [isOpen, setIsOpen] = React.useState(false);
 
-    // Render based on folder_type
-    if (template.folder_type === 'Tabs') {
-        return (
-            <div className={`folder-parm tabs ${template.runtime_data.isActive ? 'active' : ''}`}>
-                <div className="folder-content">
-                    {template.parm_templates.map((parmTemplate, index) => (
-                        <div key={template.name + index} className="parm-item">
-                            <ParmFactory data={data} parmTemplate={parmTemplate} onChange={onChange} />
-                        </div>
-                    ))}
-                </div>
-            </div>
-        );
-    } else if (template.folder_type === 'Collapsible') {
-        return (
-            <details className={`folder-parm collapsible`}>
-                <summary>{template.label}</summary>
-                <div className="folder-content">
-                    {template.parm_templates.map((parmTemplate, index) => (
-                        <div key={template.name + index} className="parm-item">
-                            <ParmFactory data={data} parmTemplate={parmTemplate} onChange={onChange} />
-                        </div>
-                    ))}
-                </div>
-            </details>
-        );
-    }
+  const handleToggle = (event: React.SyntheticEvent<HTMLDetailsElement>) => {
+    const target = event.target as HTMLDetailsElement;
+    setIsOpen(target.open);
+  };
 
-    // Default rendering if folder_type is not specifically handled
+  // Render based on folder_type
+  if (template.folder_type === 'Tabs') {
     return (
-        <div className={`folder-parm default ${template.runtime_data.isActive ? 'active' : ''}`}>
-            <div className="folder-content">
-                {template.parm_templates.map((parmTemplate, index) => (
-                    <div key={template.name + index} className="parm-item">
-                        <ParmFactory data={data} parmTemplate={parmTemplate} onChange={onChange} />
-                    </div>
-                ))}
+      <div
+        className={`folder-parm tabs ${
+          template.runtime_data.isActive ? 'active' : ''
+        }`}
+      >
+        <div className="folder-content">
+          {template.parm_templates.map((parmTemplate, index) => (
+            <div key={template.name + index} className="parm-item">
+              <ParmFactory
+                data={data}
+                parmTemplate={parmTemplate}
+                onChange={onChange}
+              />
             </div>
+          ))}
         </div>
+      </div>
     );
+  } else if (template.folder_type === 'Collapsible') {
+    return (
+      <details
+        className={`folder-parm collapsible`}
+        open={isOpen}
+        onToggle={handleToggle}
+      >
+        <summary className="cursor-pointer align-center justify-start gap-4">
+          {isOpen ? (
+            <img width="16" height="16" src={ChevronDown} alt="chevron down" />
+          ) : (
+            <img
+              width="16"
+              height="16"
+              src={ChevronRight}
+              alt="chevron right"
+            />
+          )}
+          {template.label}
+        </summary>
+        <div className="folder-content">
+          {template.parm_templates.map((parmTemplate, index) => (
+            <div key={template.name + index} className="parm-item">
+              <ParmFactory
+                data={data}
+                parmTemplate={parmTemplate}
+                onChange={onChange}
+              />
+            </div>
+          ))}
+        </div>
+      </details>
+    );
+  }
+
+  // Default rendering if folder_type is not specifically handled
+  return (
+    <div
+      className={`folder-parm default ${
+        template.runtime_data.isActive ? 'active' : ''
+      }`}
+    >
+      <div className="folder-content">
+        {template.parm_templates.map((parmTemplate, index) => (
+          <div key={template.name + index} className="parm-item">
+            <ParmFactory
+              data={data}
+              parmTemplate={parmTemplate}
+              onChange={onChange}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default FolderParm;
--- sites/libs/houdini-ui/src/components/FolderSetParm.tsx
+++ sites/libs/houdini-ui/src/components/FolderSetParm.tsx
@@ -1,51 +1,96 @@
 import React, { useState } from 'react';
-import hou,{ dictionary } from '../types/Houdini';
+import hou, { dictionary } from '../types/Houdini';
 import FolderParm from './FolderParm';
-
+import { RadioIndicator } from './RadioIndicator';
 
 export interface FolderSetParmProps {
-    template: hou.FolderSetParmTemplate;
-    data: dictionary;
-    onChange: (formData: dictionary) => void; // Callback for value changes
+  template: hou.FolderSetParmTemplate;
+  data: dictionary;
+  onChange: (formData: dictionary) => void; // Callback for value changes
 }
 
-export const FolderSetParm: React.FC<FolderSetParmProps> = ({ template, data, onChange}) => {
-    const [activeFolder, setActiveFolder] = useState<string | null>(template.parm_templates[0].name);
+export const FolderSetParm: React.FC<FolderSetParmProps> = ({
+  template,
+  data,
+  onChange,
+}) => {
+  const [activeFolder, setActiveFolder] = useState<string | null>(
+    template.parm_templates[0].name
+  );
 
-    const handleFolderActivation = (folderName: string) => {
-        setActiveFolder(folderName); // Update local active state
-    }
+  const hasCollapsibleChildren = template.parm_templates.some(
+    (parm) => parm.folder_type === 'Collapsible'
+  );
+  const hasSimpleChildren = template.parm_templates.some(
+    (parm) => parm.folder_type === 'Simple'
+  );
+  const hasRadioChildren = template.parm_templates.some(
+    (parm) => parm.folder_type === 'RadioButtons'
+  );
 
-    return (
-        <div className="folder-container">
-            {/* Tab header */}
-            <div className="folder-tabs">
-                {template.parm_templates.map((folder: hou.FolderParmTemplate, index) => (
-                    <div
-                        key={index}
-                        className={`folder-tab ${activeFolder === folder.name ? 'active' : ''}`}
-                        onClick={() => handleFolderActivation(folder.name || '')}
-                    >
-                        {folder.label}
-                    </div>
-                ))}
-            </div>
+  const handleFolderActivation = (folderName: string) => {
+    setActiveFolder(folderName); // Update local active state
+  };
 
-            {/* Only render the active folder */}
-            <div className="folder-content">
-                {template.parm_templates.map((folder: hou.FolderParmTemplate, index) => {
-                    if (folder.name === activeFolder) {
-                        return (
-                            <div key={index} className={`folder-item active`}>
-                                <FolderParm data={data} onChange={onChange} template={folder} />
-                            </div>
-                        );
-                    }
-                    return null;
-                })}
-            </div>
+  return (
+    <div
+      className={`folder-container ${
+        hasCollapsibleChildren ? 'collapsible-folder-container' : ''
+      }     
+        ${hasSimpleChildren ? 'simple-folder-container' : ''}
+      `}
+    >
+      {/* Tab header */}
+      {!hasCollapsibleChildren && (
+        <div className="folder-tabs">
+          {template.parm_templates.map(
+            (folder: hou.FolderParmTemplate, index) => (
+              <div
+                key={index}
+                className={`folder-tab ${
+                  activeFolder === folder.name ? 'active' : ''
+                } 
+                ${hasSimpleChildren ? 'simple-folder-tab' : ''}
+                ${hasRadioChildren ? 'align-center gap-4' : ''}
+                `}
+                onClick={() => handleFolderActivation(folder.name || '')}
+              >
+                {hasRadioChildren && (
+                  <RadioIndicator checked={activeFolder === folder.name} />
+                )}
+                {folder.label}
+              </div>
+            )
+          )}
         </div>
-    );
+      )}
+
+      {/* Only render the active folder */}
+      <div className="folder-content">
+        {template.parm_templates.map(
+          (folder: hou.FolderParmTemplate, index) => {
+            if (hasCollapsibleChildren || folder.name === activeFolder) {
+              return (
+                <div
+                  key={index}
+                  className={`folder-item active ${
+                    hasCollapsibleChildren ? 'collapsible-folder' : ''
+                  }`}
+                >
+                  <FolderParm
+                    data={data}
+                    onChange={onChange}
+                    template={folder}
+                  />
+                </div>
+              );
+            }
+            return null;
+          }
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default FolderSetParm;
