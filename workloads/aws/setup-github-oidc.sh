55e0c8fff0bbf8f15adf45db2123ca35ff4ee98b
Jacob Repp <jacobrepp@gmail.com>
2025-04-22 11:43:18 -0700

    make simple script to automate

---
+++ workloads/aws/setup-github-oidc.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+aws iam create-open-id-connect-provider \
+  --url https://token.actions.githubusercontent.com \
+  --client-id-list sts.amazonaws.com \
+  --thumbprint-list 6938fd4d98bab03faadb97b34396831e3780aea1 \
+  --profile mythica-admin
