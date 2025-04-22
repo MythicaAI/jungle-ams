373a80f8ae501e4c363684334bd24334dbfe06d2
pedromartello <pedro@mythica.ai>
2025-04-22 17:24:40 -0400

    [feature] Adding 3D Model gen to GenAI

--- automations/genai/workers.py
+++ automations/genai/workers.py
@@ -1,6 +1,14 @@
 import sys
 import logging
-from  automation.api import Text2MaterialRequest, Image2MaterialRequest, api_request
+from  automation.api import (
+    Text2MaterialRequest, 
+    Image2MaterialRequest,
+    Text2ThreeDRequest,
+    ThreeD2ThreeDRequest,
+    Image2ThreeDRequest,
+    MultiImage2ThreeDRequest,
+    api_request
+)
 from ripple.automation.worker import Worker
 from ripple.models.streaming import OutputFiles
 
@@ -24,6 +32,31 @@ automations = [
         "inputModel": Image2MaterialRequest,
         "outputModel": OutputFiles
     },
+    {
+        "path": '/mythica/text_2_3d',
+        "provider": api_request,
+        "inputModel": Text2ThreeDRequest,
+        "outputModel": OutputFiles
+    },
+    {
+        "path": '/mythica/3d_2_3d',
+        "provider": api_request,
+        "inputModel": ThreeD2ThreeDRequest,
+        "outputModel": OutputFiles
+    },
+    {
+        "path": '/mythica/image_2_3d',
+        "provider": api_request,
+        "inputModel": Image2ThreeDRequest,
+        "outputModel": OutputFiles
+    },
+    {
+        "path": '/mythica/multi_image_2_3d',
+        "provider": api_request,
+        "inputModel": MultiImage2ThreeDRequest,
+        "outputModel": OutputFiles
+    },
+
 ]
 
 def main():
