2b2be4f3dd06bcfc465562dfb6e7f3862973d0ba
kevin <kevin@mythica.ai>
2025-05-09 09:40:28 -0700

    Fix delete button not using shared function

--- sites/libs/houdini-ui/src/components/FolderParm.tsx
+++ sites/libs/houdini-ui/src/components/FolderParm.tsx
@@ -171,22 +171,8 @@ export const FolderParm: React.FC<FolderParmProps> = ({
             <span
               className="align-center"
               onClick={() => {
-                if (setMultiBlocks && multiBlocks) {
-                  if (multiBlocks.length > 0) {
-                    const updatedBlocks = multiBlocks.slice(0, -1);
-                    setMultiBlocks(updatedBlocks);
-
-                    const updatedValues = { ...data };
-                    const blockNameTemplate = template.parm_templates[0].name;
-
-                    const lastBlockName = blockNameTemplate.replace(
-                      /#/,
-                      `${multiBlocks.length - 1}`
-                    );
-                    updatedValues[lastBlockName] = null;
-
-                    onChange(updatedValues);
-                  }
+                if (multiBlocks?.length) {
+                  handleRemoveMultiBlock(multiBlocks.length - 1);
                 }
               }}
               style={{ paddingLeft: '2px' }}
