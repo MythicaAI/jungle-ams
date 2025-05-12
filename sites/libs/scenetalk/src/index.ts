243b06303d4f62d1dbcf6f78baf2b88913f48cdc
pedromartello <pedro@mythica.ai>
2025-05-12 18:11:31 -0400

    [scenetalk] Add SceneTalkConnector component to make UI easier to assemble.

--- sites/libs/scenetalk/src/index.ts
+++ sites/libs/scenetalk/src/index.ts
@@ -1,3 +1,4 @@
 export { SceneTalkConnection } from "./sceneTalkConnection";
 export { useSceneStore } from "./sceneStore";
+export { SceneTalkConnector } from "./sceneTalkConnector";
 export type { InputFile, MeshData, ConnectionStatus } from "./types";
