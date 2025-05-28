f147711b8ae806eaedec61a368051f740ee70a13
Pedro Martello <pedro@mythica.ai>
2025-05-28 08:11:06 -0700

    [awful-ui] fix react in jsx scope lint issue (#1001)
    - re-enable strict mode

--- sites/awful-ui/src/main.tsx
+++ sites/awful-ui/src/main.tsx
@@ -6,5 +6,7 @@ import App from './App';
 import './styles/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <App />
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
