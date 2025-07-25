ec55370cd760ef5c3c83d54f54ba20fc31fac20c
Jacob Repp <jacobrepp@gmail.com>
2025-07-25 15:17:15 -0700

    [gha] add image pushing workflow (#1024)
    * add image pushing workflow
    
    * simplify paths and deployment, use docker hub
    
    * fixing ci workflows
    
    * add pypi publish targets
    
    * fixing security issues
    
    * update permissions in new workflows

--- api/docker-entrypoint.sh
+++ api/docker-entrypoint.sh
@@ -7,6 +7,14 @@ set -eof pipefail
 
 ./print_ip_address.py
 
+if [ "$1" == "packager" ]; then
+  echo "running in packager mode"
+  python3 packager.py \
+    --endpoint=${MYTHICA_ENDPOINT} \
+    --api_key=${MYTHICA_API_KEY} $*
+  exit 0
+fi
+
 # Run DB migration to ensure database is at latest revision
 alembic upgrade head
 
